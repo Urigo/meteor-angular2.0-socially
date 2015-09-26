5e617296e17f9f0f5b30d8b50571ac85ecdae9b8
barbatus <alex.borodach@gmail.com>
2015-09-26 17:32:31 +0300

    Step 2.2: load NgFor directive, add it to the View's directives list

--- client/app.ts
+++ client/app.ts
@@ -1,12 +1,13 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
 
-import {Component, View, bootstrap} from 'angular2/angular2';
+import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
 
 @Component({
     selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html'
+    templateUrl: 'client/app.html',
+    directives: [NgFor]
 })
 class Socially { }
 
