6f29facacb9ebd0db4838be3bf4176eda54b24a0
barbatus <alex.borodach@gmail.com>
2015-09-26 17:34:52 +0300

    Step 2.3: load parties data into the app

--- client/app.ts
+++ client/app.ts
@@ -9,6 +9,23 @@ import {Component, View, NgFor, bootstrap} from 'angular2/angular2';
     templateUrl: 'client/app.html',
     directives: [NgFor]
 })
-class Socially { }
+class Socially {
+    constructor () {
+        this.parties = [
+          {'name': 'Dubstep-Free Zone',
+           'description': 'Can we please just for an evening not listen to dubstep.',
+           'location': 'Palo Alto'
+          },
+          {'name': 'All dubstep all the time',
+           'description': 'Get it on!',
+           'location': 'Palo Alto'
+          },
+          {'name': 'Savage lounging',
+           'description': 'Leisure suit required. And only fiercest manners.',
+           'location': 'San Francisco'
+          }
+        ];
+    }
+}
 
 bootstrap(Socially);
