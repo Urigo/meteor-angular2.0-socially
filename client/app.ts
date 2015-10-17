3583c48c6c982bbe443d32f3261e74df7b1af091
barbatus <alex.borodach@gmail.com>
2015-10-18 02:19:18 +0300

    Step 5.7:  configure routes

--- client/app.ts
+++ client/app.ts
@@ -1,17 +1,26 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
 
-import {Component, View, NgFor} from 'angular2/angular2';
+import {Component, View, NgFor, provide} from 'angular2/angular2';
 
 import {bootstrap} from 'angular2-meteor';
 
-import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
+import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig, APP_BASE_HREF} from 'angular2/router';
+
+import {PartiesList} from 'client/parties-list/parties-list';
+
+import {PartyDetails} from 'client/party-details/party-details';
 
 @Component({
     selector: 'app'
 })
 @View({
+    template: '<router-outlet></router-outlet>',
     directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+    { path: '/', as: 'PartiesList', component: PartiesList },
+    { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+])
 class Socially {}
 
-bootstrap(Socially, [ROUTER_PROVIDERS]);
+bootstrap(Socially, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
