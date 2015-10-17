c9222226b706654f128ef59afe5235389748e347
barbatus <alex.borodach@gmail.com>
2015-10-18 02:03:06 +0300

    Step 5.4:  clean up Socally component

--- client/app.ts
+++ client/app.ts
@@ -2,11 +2,7 @@
 
 import {Component, View, NgFor} from 'angular2/angular2';
 
-import {Parties} from 'collections/parties';
-
 import {bootstrap} from 'angular2-meteor';
-
-import {PartiesForm} from 'client/parties-form/parties-form';
 
 import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 
@@ -14,19 +10,8 @@ import {ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router'
     selector: 'app'
 })
 @View({
-    templateUrl: 'client/app.html',
-    directives: [NgFor, PartiesForm, ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES]
 })
-class Socially {
-    parties: Mongo.Cursor<Object>;
-
-    constructor () {
-        this.parties = Parties.find();
-    }
-
-    removeParty(party) {
-        Parties.remove(party._id);
-    }
-}
+class Socially {}
 
 bootstrap(Socially, [ROUTER_PROVIDERS]);
