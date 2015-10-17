67dcba79ec8e050d9e5c186554197eb711d9923c
barbatus <alex.borodach@gmail.com>
2015-10-17 17:32:28 +0300

    Step 4.5:  import dependencies and add form directives to the View

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -2,10 +2,13 @@
 
 import {Component, View} from 'angular2/angular2';
 
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/angular2';
+
 @Component({
     selector: 'parties-form'
 })
 @View({
-    templateUrl: 'client/parties-form/parties-form.html'
+    templateUrl: 'client/parties-form/parties-form.html',
+    directives: [FORM_DIRECTIVES]
 })
 export class PartiesForm { }
