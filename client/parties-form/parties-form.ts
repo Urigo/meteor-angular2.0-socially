580fb5a5bd9278c55a9cd5ad2fea264177d8b10b
barbatus <alex.borodach@gmail.com>
2015-10-17 14:58:32 +0300

    Step 4.1:  create PartiesForm component

---
+++ client/parties-form/parties-form.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../typings/angular2-meteor.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+    selector: 'parties-form'
+})
+@View({
+    templateUrl: 'client/parties-form/parties-form.html'
+})
+export class PartiesForm { }
