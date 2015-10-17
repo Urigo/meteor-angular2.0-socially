0702de737f394a339e587d6dfb08f717f0293fe9
barbatus <alex.borodach@gmail.com>
2015-10-18 02:14:40 +0300

    Step 5.5:  add PartyDetails component

---
+++ client/party-details/party-details.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../typings/angular2-meteor.d.ts" />
+
+import {Component, View} from 'angular2/angular2';
+
+@Component({
+  selector: 'party-details'
+})
+@View({
+  templateUrl: '/client/party-details/party-details.html'
+})
+export class PartyDetails {}
