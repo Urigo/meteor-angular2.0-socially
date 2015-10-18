ef43f0eb176eb5da1d43a9143b8b85ae614885bd
barbatus <alex.borodach@gmail.com>
2015-10-18 20:11:55 +0300

    Step 5.10:  injecting route parameters

--- client/party-details/party-details.ts
+++ client/party-details/party-details.ts
@@ -2,10 +2,16 @@
 
 import {Component, View} from 'angular2/angular2';
 
+import {RouteParams} from 'angular2/router';
+
 @Component({
-  selector: 'party-details'
+    selector: 'party-details'
 })
 @View({
-  templateUrl: '/client/party-details/party-details.html'
+    templateUrl: '/client/party-details/party-details.html'
 })
-export class PartyDetails {}
+export class PartyDetails {
+    constructor(params: RouteParams) {
+        var partyId = params.get('partyId');
+    }
+}
