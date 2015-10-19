ce0ca1c92b91f4f4aff85454c2065107b2fc8111
barbatus <alex.borodach@gmail.com>
2015-10-19 13:28:41 +0300

    Step 3.4: fetch parties

--- client/app.ts
+++ client/app.ts
@@ -15,20 +15,7 @@ class Socially {
     parties: Array<Object>;
 
     constructor () {
-        this.parties = [
-          {'name': 'Dubstep-Free Zone',
-           'description': 'Can we please just for an evening not listen to dubstep.',
-           'location': 'Palo Alto'
-          },
-          {'name': 'All dubstep all the time',
-           'description': 'Get it on!',
-           'location': 'Palo Alto'
-          },
-          {'name': 'Savage lounging',
-           'description': 'Leisure suit required. And only fiercest manners.',
-           'location': 'San Francisco'
-          }
-        ];
+        this.parties = Parties.find().fetch();
     }
 }
 
