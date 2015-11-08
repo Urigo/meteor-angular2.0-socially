27c47be2531003b3a6b0b036a4328220eebb97b9
barbatus <alex.borodach@gmail.com>
2015-11-08 23:23:24 +0300

    Adds RouterLink dependency.

--- client/party-details/party-details.ts
+++ client/party-details/party-details.ts
@@ -6,11 +6,14 @@ import {RouteParams} from 'angular2/router';
 
 import {Parties} from 'collections/parties';
 
+import {RouterLink} from 'angular2/router';
+
 @Component({
     selector: 'party-details'
 })
 @View({
-    templateUrl: '/client/party-details/party-details.html'
+    templateUrl: '/client/party-details/party-details.html',
+    directives: [RouterLink]
 })
 export class PartyDetails {
     party: Object;
