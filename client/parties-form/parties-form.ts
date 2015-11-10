8d0584729e1f94e40172717dd068ede492b3fdfa
barbatus <alex.borodach@gmail.com>
2015-11-10 18:34:06 +0300

    Step 8.4: check access and add party owner ID along with other data

--- client/parties-form/parties-form.ts
+++ client/parties-form/parties-form.ts
@@ -30,15 +30,20 @@ export class PartiesForm {
 
     addParty(party) {
         if (this.partiesForm.valid) {
-            Parties.insert({
-                name: party.name,
-                description: party.description,
-                location: party.location
-            });
+            if (Meteor.userId()) {
+                Parties.insert({
+                    name: party.name,
+                    description: party.description,
+                    location: party.location,
+                    owner: Meteor.userId()
+                });
 
-            (<Control>this.partiesForm.controls['name']).updateValue('');
-            (<Control>this.partiesForm.controls['description']).updateValue('');
-            (<Control>this.partiesForm.controls['location']).updateValue('');
+                (<Control>this.partiesForm.controls['name']).updateValue('');
+                (<Control>this.partiesForm.controls['description']).updateValue('');
+                (<Control>this.partiesForm.controls['location']).updateValue('');
+            } else {
+                alert('Please log in to add a party');
+            }
         }
     }
 }
