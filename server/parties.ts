3c6bf7d2075830e310bc3d706ba477ad2c6e8daf
barbatus <alex.borodach@gmail.com>
2015-11-13 18:36:07 +0300

    Step 9.8: add "party" publication

--- server/parties.ts
+++ server/parties.ts
@@ -2,16 +2,30 @@
 
 import {Parties} from 'collections/parties';
 
-Meteor.publish('parties', function() {
-    return Parties.find({
+function buildQuery(partyId?: string): Object {
+    var isAvailable = {
         $or: [
             { public: true },
             {
                 $and: [
-                  { owner: this.userId },
-                  { owner: { $exists: true } }
+                    { owner: this.userId },
+                    { owner: { $exists: true } }
                 ]
             }
         ]
-    });
+    };
+
+    if (partyId) {
+        return { $and: [{ _id: partyId }, isAvailable] };
+    }
+
+    return isAvailable;
+}
+
+Meteor.publish('parties', function() {
+    return Parties.find(buildQuery.call(this));
+});
+
+Meteor.publish('party', function(partyId) {
+    return Parties.find(buildQuery.call(this, partyId));
 });
