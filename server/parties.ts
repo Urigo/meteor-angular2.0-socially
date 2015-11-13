d9599c82bf134b4f263957bdd435f2b25707003e
barbatus <alex.borodach@gmail.com>
2015-11-13 18:13:29 +0300

    Step 9.7: limit data sent to the client

--- server/parties.ts
+++ server/parties.ts
@@ -3,5 +3,15 @@
 import {Parties} from 'collections/parties';
 
 Meteor.publish('parties', function() {
-    return Parties.find();
+    return Parties.find({
+        $or: [
+            { public: true },
+            {
+                $and: [
+                  { owner: this.userId },
+                  { owner: { $exists: true } }
+                ]
+            }
+        ]
+    });
 });
