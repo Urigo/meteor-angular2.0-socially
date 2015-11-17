e3176b248945a9bd415284d517682166c2898308
barbatus <alex.borodach@gmail.com>
2015-11-17 19:37:25 +0300

    Step 7.2: define parties collection with Party type

--- collections/parties.ts
+++ collections/parties.ts
@@ -1,3 +1,4 @@
 /// <reference path="../typings/angular2-meteor.d.ts" />
+/// <reference path="../typings/party.d.ts" />
 
-export var Parties = new Mongo.Collection('parties');
+export var Parties = new Mongo.Collection<Party>('parties');
