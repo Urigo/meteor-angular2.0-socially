1e7a983ee5611c20f089dab0ec074d718715fe7d
Dotan Simha <dotansimha@gmail.com>
2016-01-25 15:50:04 +0200

    Step 2.2: load parties data into the app

--- client/app.ts
+++ client/app.ts
@@ -7,6 +7,23 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
   selector: 'app',
   templateUrl: 'client/app.html'
 })
-class Socially { }
+class Socially {
+  constructor() {
+    this.parties = [
+      {'name': 'Dubstep-Free Zone',
+        'description': 'Can we please just for an evening not listen to dubstep.',
+        'location': 'Palo Alto'
+      },
+      {'name': 'All dubstep all the time',
+        'description': 'Get it on!',
+        'location': 'Palo Alto'
+      },
+      {'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.',
+        'location': 'San Francisco'
+      }
+    ];
+  }
+}
 
 bootstrap(Socially);
