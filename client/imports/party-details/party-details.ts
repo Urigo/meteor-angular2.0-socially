c3960f5ef12b3b856620c90716decd57a81652a5
Dotan Simha <dotansimha@gmail.com>
2016-01-25 17:36:57 +0200

    Step 5.13: load party details

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Parties} from '../../../collections/parties.ts';
 
 import template from './party-details.html';
 
@@ -9,12 +10,15 @@ import template from './party-details.html';
 })
 export class PartyDetails {
   partyId: string;
+  party: Object;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.partyId = params['partyId'];
+
+      this.party = Parties.findOne(this.partyId);
     });
   }
 }
