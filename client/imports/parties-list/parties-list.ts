4c4b5c1214309ccde908817ca68edf4108af12be
Dotan Simha <dotansimha@gmail.com>
2016-01-25 17:34:22 +0200

    Step 5.10: import RouterLink directive

--- client/imports/parties-list/parties-list.ts
+++ client/imports/parties-list/parties-list.ts
@@ -2,11 +2,13 @@ import { Component }   from '@angular/core';
 import { Parties }     from '../../../collections/parties';
 import { PartiesForm } from '../parties-form/parties-form';
 import { Mongo }       from 'meteor/mongo';
+import { RouterLink }  from '@angular/router-deprecated';
+
 
 @Component({
   selector: 'parties-list',
   templateUrl: '/client/imports/parties-list/parties-list.html',
-  directives: [PartiesForm]
+  directives: [PartiesForm, RouterLink]
 })
 export class PartiesList {
   parties: Mongo.Cursor;
