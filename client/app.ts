bfdac5e8c231aef9680ee14e27e295f105615106
Dotan Simha <dotansimha@gmail.com>
2016-01-25 17:32:05 +0200

    Step 5.8: configure routes

--- client/app.ts
+++ client/app.ts
@@ -1,14 +1,21 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
+import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from '@angular/router-deprecated';
+import { APP_BASE_HREF } from '@angular/common';
+import { PartiesList } from './imports/parties-list/parties-list.ts';
+import { PartyDetails } from './imports/party-details/party-details.ts';
 
 @Component({
   selector: 'app',
   templateUrl: 'client/app.html',
   directives: [ROUTER_DIRECTIVES]
 })
+@RouteConfig([
+  { path: '/', as: 'PartiesList', component: PartiesList },
+  { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+])
 class Socially {}
 
-bootstrap(Socially, [ROUTER_PROVIDERS]);
+bootstrap(Socially, [ROUTER_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
