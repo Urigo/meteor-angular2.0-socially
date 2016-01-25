029d92a71ecf10bc734e10878b51421778ec9cd1
Dotan Simha <dotansimha@gmail.com>
2016-01-25 16:14:47 +0200

    Step 4.4: import the PartiesForm component and link it with the Socially component

--- client/app.ts
+++ client/app.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { Parties }   from '../collections/parties';
 import { Mongo }     from 'meteor/mongo';
+import { PartiesForm } from './imports/parties-form/parties-form';
 
 import template from './app.html';
 
 @Component({
   selector: 'app',
-  template
+  template,
+  directives: [PartiesForm]
 })
 class Socially {
   parties: Mongo.Cursor<Object>;
