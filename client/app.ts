b82d165e3751593ee4d8f58a964c0731c10c5314
Dotan Simha <dotansimha@gmail.com>
2016-01-25 16:08:10 +0200

    Step 3.5: Bind Mongo Collection to Angular

--- client/app.ts
+++ client/app.ts
@@ -2,29 +2,17 @@ import 'reflect-metadata';
 import { Component } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { Parties }   from '../collections/parties';
+import { Mongo }     from 'meteor/mongo';
 
 @Component({
   selector: 'app',
   templateUrl: '/client/app.html'
 })
 class Socially {
-  parties: Array<Object>;
+  parties: Mongo.Cursor<Object>;
 
-  constructor() {
-    this.parties = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto'
-      },
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      }
-    ];
+  constructor () {
+    this.parties = Parties.find();
   }
 }
 
