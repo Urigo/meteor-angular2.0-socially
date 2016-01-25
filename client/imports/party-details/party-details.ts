449fe016fcd42a85fd37cdd4e6e78f68abeaa7e3
Dotan Simha <dotansimha@gmail.com>
2016-01-25 17:36:57 +0200

    Step 5.13: load party details

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouteParams } from '@angular/router-deprecated';
+import {Parties} from '../../../collections/parties.ts';
 
 @Component({
   selector: 'party-details',
   templateUrl: '/client/imports/party-details/party-details.html'
 })
 export class PartyDetails {
+  party: Object;
+
   constructor(params: RouteParams) {
     var partyId = params.get('partyId');
+    this.party = Parties.findOne(partyId);
   }
 }
