37dcd0e2801c6c246468d4a14acee9024f08a4c9
Dotan Simha <dotansimha@gmail.com>
2016-01-25 16:26:07 +0200

    Step 5.4: Clean up app.ts file

--- client/app.ts
+++ client/app.ts
@@ -1,9 +1,6 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
-import { Parties }   from '../collections/parties';
-import { Mongo }     from 'meteor/mongo';
-import { PartiesForm } from './imports/parties-form/parties-form';
 import { provideRouter, RouterConfig, ROUTER_DIRECTIVES } from '@angular/router';
 
 import template from './app.html';
@@ -11,18 +8,8 @@ import template from './app.html';
 @Component({
   selector: 'app',
   template,
-  directives: [PartiesForm, ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES]
 })
-class Socially {
-  parties: Mongo.Cursor<Object>;
-
-  constructor () {
-    this.parties = Parties.find();
-  }
-
-  removeParty(party) {
-    Parties.remove(party._id);
-  }
-}
+class Socially {}
 
 bootstrap(Socially);
