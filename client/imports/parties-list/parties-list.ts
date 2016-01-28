d65baa7b615404962c44a5c6ba641fc820ca9657
Dotan Simha <dotansimha@gmail.com>
2016-01-28 23:16:21 +0200

    Step 8.3: import dependencies

--- client/imports/parties-list/parties-list.ts
+++ client/imports/parties-list/parties-list.ts
@@ -3,12 +3,12 @@ import { Parties }     from '../../../collections/parties';
 import { PartiesForm } from '../parties-form/parties-form';
 import { Mongo }       from 'meteor/mongo';
 import { RouterLink }  from '@angular/router-deprecated';
-
+import { LoginButtons } from 'angular2-meteor-accounts-ui';
 
 @Component({
   selector: 'parties-list',
   templateUrl: '/client/imports/parties-list/parties-list.html',
-  directives: [PartiesForm, RouterLink]
+  directives: [PartiesForm, RouterLink, LoginButtons]
 })
 export class PartiesList {
   parties: Mongo.Cursor<Party>;
