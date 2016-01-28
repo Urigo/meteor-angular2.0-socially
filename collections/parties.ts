a765faf10f0e2b94157ff57341435f291cb86623
Dotan Simha <dotansimha@gmail.com>
2016-01-28 23:16:57 +0200

    Step 8.5: add parties collection security

--- collections/parties.ts
+++ collections/parties.ts
@@ -1,3 +1,22 @@
 import {Mongo} from 'meteor/mongo';
+import {Meteor} from 'meteor/meteor';
 
 export let Parties = new Mongo.Collection<Party>('parties');
+
+Parties.allow({
+  insert: function() {
+    let user = Meteor.user();
+    
+    return !!user;
+  },
+  update: function() {
+    let user = Meteor.user();
+    
+    return !!user;
+  },
+  remove: function() {
+    let user = Meteor.user();
+    
+    return !!user;
+  }
+});
