eb505555122f42bb820daacc53561c159cc897d0
Dotan Simha <dotansimha@gmail.com>
2016-01-28 23:26:35 +0200

    Step 9.6: add public field to the initial parties

--- server/load-parties.ts
+++ server/load-parties.ts
@@ -7,17 +7,20 @@ export function loadParties() {
       {
         'name': 'Dubstep-Free Zone',
         'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto'
+        'location': 'Palo Alto',
+        'public': true
       },
       {
         'name': 'All dubstep all the time',
         'description': 'Get it on!',
-        'location': 'Palo Alto'
+        'location': 'Palo Alto',
+        'public': true
       },
       {
         'name': 'Savage lounging',
         'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
+        'location': 'San Francisco',
+        'public': false
       }
     ];
 
