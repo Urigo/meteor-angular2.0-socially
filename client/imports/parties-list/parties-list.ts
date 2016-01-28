d55fb6cf3b8937aad34dccdee27b2d9853648132
Dotan Simha <dotansimha@gmail.com>
2016-01-28 23:24:12 +0200

    Step 9.3: extend PartiesList and make use of subscribe method

--- client/imports/parties-list/parties-list.ts
+++ client/imports/parties-list/parties-list.ts
@@ -4,6 +4,7 @@ import { PartiesForm } from '../parties-form/parties-form';
 import { Mongo }       from 'meteor/mongo';
 import { ROUTER_DIRECTIVES }  from '@angular/router';
 import { LoginButtons } from 'angular2-meteor-accounts-ui';
+import { MeteorComponent } from 'angular2-meteor';
 
 import template from './parties-list.html';
 
@@ -12,11 +13,14 @@ import template from './parties-list.html';
   template,
   directives: [PartiesForm, ROUTER_DIRECTIVES, LoginButtons]
 })
-export class PartiesList {
+export class PartiesList extends MeteorComponent{
   parties: Mongo.Cursor<Party>;
 
   constructor() {
-    this.parties = Parties.find();
+    super();
+    this.subscribe('parties', () => {
+      this.parties = Parties.find();
+    }, true);
   }
 
   removeParty(party) {
