01e66eb8b3b0b1e28462ee9d0a4f32d13989f20b
Dotan Simha <dotansimha@gmail.com>
2016-01-28 23:33:12 +0200

    Step 12.2: create options and add them to the parties subscription

--- client/imports/parties-list/parties-list.ts
+++ client/imports/parties-list/parties-list.ts
@@ -13,10 +13,20 @@ import { MeteorComponent } from 'angular2-meteor';
 })
 export class PartiesList extends MeteorComponent{
   parties: Mongo.Cursor<Party>;
+  pageSize: number = 10;
+  curPage: number = 1;
+  nameOrder: number = 1;
 
   constructor() {
     super();
-    this.subscribe('parties', () => {
+
+    let options = {
+      limit: this.pageSize,
+      skip: (this.curPage - 1) * this.pageSize,
+      sort: { name: this.nameOrder }
+    };
+
+    this.subscribe('parties', options, () => {
       this.parties = Parties.find();
     }, true);
   }
