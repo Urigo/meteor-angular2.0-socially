bc2dc4411648c0d20bc4e60d60527d9e6534f876
Dotan Simha <dotansimha@gmail.com>
2016-02-03 22:51:49 +0200

    Step 14.2: import methods on the server side

--- server/main.ts
+++ server/main.ts
@@ -2,5 +2,6 @@ import {loadParties} from './load-parties.ts';
 import {Meteor} from 'meteor/meteor';
 import './parties.ts';
 import './users.ts';
+import '../collections/methods.ts';
 
 Meteor.startup(loadParties);
