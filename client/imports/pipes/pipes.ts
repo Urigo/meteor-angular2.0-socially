b56c1574656f367eee23ab1fca478cd9186f5a4c
Dotan Simha <dotansimha@gmail.com>
2016-02-03 23:21:50 +0200

    Step 14.13: add a new response counting pipe

--- client/imports/pipes/pipes.ts
+++ client/imports/pipes/pipes.ts
@@ -1,6 +1,8 @@
 import 'reflect-metadata';
 import { Pipe } from '@angular/core';
 import { Meteor } from 'meteor/meteor';
+import { Parties } from '../../../collections/parties.ts';
+import { MeteorComponent } from 'angular2-meteor';
 
 @Pipe({
   name: 'displayName'
@@ -20,5 +22,34 @@ export class DisplayName {
     }
 
     return '';
+  }
+}
+
+
+@Pipe({
+  name: 'rsvp',
+  pure: false
+})
+export class RsvpPipe extends MeteorComponent {
+  init: boolean = false;
+  total: number = 0;
+
+  transform(party: Party, type: string): number {
+    if (!type) {
+      return 0;
+    }
+
+    if (!this.init) {
+      this.autorun(() => {
+        party = Parties.findOne(party._id);
+        if (party) {
+          this.total = party.rsvps ?
+            party.rsvps.filter(rsvp => rsvp.response === type).length : 0;
+        }
+      }, true);
+      this.init = true;
+    }
+
+    return this.total;
   }
 }
