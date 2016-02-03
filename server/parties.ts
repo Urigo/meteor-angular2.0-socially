1cee50a8ff7a4aa6623b702e474cd5c5807ad778
Dotan Simha <dotansimha@gmail.com>
2016-02-03 22:27:25 +0200

    Step 12.18: add search by the party location using Mongo regex

--- server/parties.ts
+++ server/parties.ts
@@ -2,10 +2,10 @@ import {Parties} from '../collections/parties';
 import {Meteor} from 'meteor/meteor';
 import {Counts} from 'meteor/tmeasday:publish-counts';
 
-function buildQuery(partyId?: string): Object {
+function buildQuery(partyId: string, location: string): Object {
   var isAvailable = {
     $or: [
-      { 'public': true },
+      { public: true },
       {
         $and: [
           { owner: this.userId },
@@ -19,13 +19,15 @@ function buildQuery(partyId?: string): Object {
     return { $and: [{ _id: partyId }, isAvailable] };
   }
 
-  return isAvailable;
+  let searchRegEx = { '$regex': '.*' + (location || '') + '.*', '$options': 'i' };
+
+  return { $and: [{ location: searchRegEx }, isAvailable] };
 }
 
-Meteor.publish('parties', function(options: Object) {
-  Counts.publish(this, 'numberOfParties', Parties.find(buildQuery.call(this)), { noReady: true });
-
-  return Parties.find(buildQuery.call(this), options);
+Meteor.publish('parties', function(options: Object, location: string) {
+  Counts.publish(this, 'numberOfParties',
+    Parties.find(buildQuery.call(this, null, location)), { noReady: true });
+  return Parties.find(buildQuery.call(this, null, location), options);
 });
 
 Meteor.publish('party', function(partyId: string) {
