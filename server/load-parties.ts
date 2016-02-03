6f4bd620016dbc2b0c113f0889a83d042508b9e9
Dotan Simha <dotansimha@gmail.com>
2016-02-04 00:34:28 +0200

    Step 16.2: change initial parties accordingly

--- server/load-parties.ts
+++ server/load-parties.ts
@@ -7,20 +7,26 @@ export function loadParties() {
       {
         'name': 'Dubstep-Free Zone',
         'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto',
-        'public': true
+        'public': true,
+        'location': {
+          name: 'Palo Alto'
+        }
       },
       {
         'name': 'All dubstep all the time',
         'description': 'Get it on!',
-        'location': 'Palo Alto',
-        'public': true
+        'public': true,
+        'location': {
+          name: 'Palo Alto'
+        }
       },
       {
         'name': 'Savage lounging',
         'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco',
-        'public': false
+        'public': false,
+        'location': {
+          name: 'Palo Alto'
+        }
       }
     ];
 
