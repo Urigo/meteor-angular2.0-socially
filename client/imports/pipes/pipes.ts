7c7ae83fa8d17e58f977154247b4ae8b0f845deb
Dotan Simha <dotansimha@gmail.com>
2016-02-03 22:45:15 +0200

    Step 13.6: add new pipe to render user name

---
+++ client/imports/pipes/pipes.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import { Pipe } from '@angular/core';
+import { Meteor } from 'meteor/meteor';
+
+@Pipe({
+  name: 'displayName'
+})
+export class DisplayName {
+  transform(user: Meteor.User): string {
+    if (!user) {
+      return '';
+    }
+
+    if (user.username) {
+      return user.username;
+    }
+
+    if (user.emails) {
+      return user.emails[0].address;
+    }
+
+    return '';
+  }
+}
