64d0aac1555fbf1aef8130ff0fa41676e854bb13
Dotan Simha <dotansimha@gmail.com>
2016-02-06 13:38:32 +0200

    Step 18.14: Add logic to support custom login

--- client/app.ts
+++ client/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {MATERIAL_PROVIDERS, MATERIAL_DIRECTIVES} from 'ng2-material';
 import {MdToolbar} from '@angular2-material/toolbar';
+import {MeteorComponent} from 'angular2-meteor';
 
 import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
@@ -12,17 +13,36 @@ import { PartyDetails } from './imports/party-details/party-details.ts';
 import '../collections/methods.ts';
 import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 import {LoginButtons} from 'angular2-meteor-accounts-ui';
-import '../node_modules/@angular2-material/toolbar/toolbar.css'
+import '../node_modules/@angular2-material/toolbar/toolbar.css';
+import {RouterLink} from '@angular/router-deprecated';
+import {DisplayName} from './imports/pipes/pipes.ts';
+import {InjectUser} from 'angular2-meteor-accounts-ui';
+import {Login} from './imports/auth/login.ts';
+import {Signup} from './imports/auth/signup.ts';
+import {Recover} from './imports/auth/recover.ts';
+
+
 
 @Component({
   selector: 'app',
   templateUrl: 'client/app.html',
-  directives: [ROUTER_DIRECTIVES, LoginButtons, MATERIAL_DIRECTIVES, MdToolbar]
+  directives: [ROUTER_DIRECTIVES, LoginButtons, MATERIAL_DIRECTIVES, MdToolbar, RouterLink],
+  pipes: [DisplayName]
 })
 @RouteConfig([
   { path: '/', as: 'PartiesList', component: PartiesList },
-  { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails }
+  { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails },
+  { path: '/login', as: 'Login', component: Login },
+  { path: '/signup', as: 'Signup', component: Signup },
+  { path: '/recover', as: 'Recover', component: Recover }
 ])
-class Socially {}
+@InjectUser()
+class Socially extends MeteorComponent {
+  user: Meteor.User;
+
+  constructor() {
+    super();
+  }
+}
 
 bootstrap(Socially, [MATERIAL_PROVIDERS, ROUTER_PROVIDERS, ANGULAR2_GOOGLE_MAPS_PROVIDERS, provide(APP_BASE_HREF, { useValue: '/' })]);
