238fc8a04c78130024790436354d8126b221afb6
Dotan Simha <dotansimha@gmail.com>
2016-02-06 10:33:40 +0200

    Step 18.5: Change the nav bar and the layout

--- client/app.ts
+++ client/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import {MATERIAL_PROVIDERS, MATERIAL_DIRECTIVES} from 'ng2-material';
+import {MdToolbar} from '@angular2-material/toolbar';
 
 import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
@@ -10,11 +11,12 @@ import { PartyDetails } from './imports/party-details/party-details.ts';
 import '../collections/methods.ts';
 import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 import {LoginButtons} from 'angular2-meteor-accounts-ui';
+import '../node_modules/@angular2-material/toolbar/toolbar.css'
 
 @Component({
   selector: 'app',
   templateUrl: '/client/app.html',
-  directives: [ROUTER_DIRECTIVES, LoginButtons, MATERIAL_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, LoginButtons, MATERIAL_DIRECTIVES, MdToolbar]
 })
 @RouteConfig([
   { path: '/', as: 'PartiesList', component: PartiesList },
