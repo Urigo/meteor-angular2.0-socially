d0b4130bd19f4e824d4685787e0984763612cf71
Dotan Simha <dotansimha@gmail.com>
2016-02-06 14:11:40 +0200

    Step 18.15: Create the login component

---
+++ client/imports/auth/login.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { MeteorComponent } from 'angular2-meteor';
+import { FormBuilder, ControlGroup, Validators } from '@angular/common';
+import { MATERIAL_DIRECTIVES } from 'ng2-material';
+import { Router , ROUTER_DIRECTIVES} from '@angular/router';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { Meteor } from 'meteor/meteor';
+
+@Component({
+  selector: 'login',
+  directives: [MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES, MD_INPUT_DIRECTIVES, MdToolbar],
+  templateUrl: '/client/imports/auth/login.html'
+})
+export class Login extends MeteorComponent {
+  loginForm: ControlGroup;
+  error: string;
+
+  constructor(private router: Router) {
+    super();
+
+    let fb = new FormBuilder();
+
+    this.loginForm = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.error = '';
+  }
+
+  login(credentials) {
+    if (this.loginForm.valid) {
+      Meteor.loginWithPassword(credentials.email, credentials.password, (err) => {
+        if (err) {
+          this.error = err;
+        }
+        else {
+          this.router.navigate(['/']);
+        }
+      });
+    }
+  }
+}
