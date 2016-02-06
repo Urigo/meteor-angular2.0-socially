3c0993f5b482707c0e9320500080e65669e4b2c7
Dotan Simha <dotansimha@gmail.com>
2016-02-06 13:38:32 +0200

    Step 18.14: Add logic to support custom login

--- client/app.ts
+++ client/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import {MATERIAL_PROVIDERS, MATERIAL_DIRECTIVES} from 'ng2-material';
 import {MdToolbar} from '@angular2-material/toolbar';
+import {MeteorComponent} from 'angular2-meteor';
 
 import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
@@ -11,20 +12,37 @@ import { PartyDetails } from './imports/party-details/party-details.ts';
 import '../collections/methods.ts';
 import {GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 import {LoginButtons} from 'angular2-meteor-accounts-ui';
-import '../node_modules/@angular2-material/toolbar/toolbar.css'
+import '../node_modules/@angular2-material/toolbar/toolbar.css';
+import { DisplayName } from './imports/pipes/pipes';
+import { InjectUser } from 'angular2-meteor-accounts-ui';
+import { Login } from './imports/auth/login';
+import { Signup } from './imports/auth/signup';
+import { Recover } from './imports/auth/recover';
+import { Meteor } from 'meteor/meteor';
 
 import template from './app.html';
 
 @Component({
   selector: 'app',
   template,
-  directives: [ROUTER_DIRECTIVES, LoginButtons, MATERIAL_DIRECTIVES, MdToolbar]
+  directives: [ROUTER_DIRECTIVES, LoginButtons, MATERIAL_DIRECTIVES, MdToolbar],
+  pipes: [DisplayName]
 })
-class Socially {}
+@InjectUser()
+class Socially extends MeteorComponent {
+  user: Meteor.User;
+
+  constructor() {
+    super();
+  }
+}
 
 const routes: RouterConfig = [
   { path: '',              	component: PartiesList },
   { path: 'party/:partyId',	component: PartyDetails },
+  { path: 'login', 		component: Login },
+  { path: 'signup', 		component: Signup },
+  { path: 'recover', 		component: Recover }
 ];
 
 const APP_ROUTER_PROVIDERS = [
