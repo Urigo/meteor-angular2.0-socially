2447124c2976f9e861cf3f967ac5de1404f4bf62
Dotan Simha <dotansimha@gmail.com>
2016-02-06 14:12:04 +0200

    Step 18.17: Create the signup component

---
+++ client/imports/auth/signup.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { MeteorComponent } from 'angular2-meteor';
+import { FormBuilder, ControlGroup, Validators } from '@angular/common';
+import { MATERIAL_DIRECTIVES } from 'ng2-material';
+import { Router , RouterLink} from '@angular/router-deprecated';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { Accounts } from 'meteor/accounts-base';
+
+@Component({
+  selector: 'signup',
+  directives: [MATERIAL_DIRECTIVES, RouterLink, MD_INPUT_DIRECTIVES, MdToolbar],
+  templateUrl: '/client/imports/auth/signup.html'
+})
+export class Signup extends MeteorComponent {
+  signupForm: ControlGroup;
+  error: string;
+
+  constructor(private router: Router) {
+    super();
+
+    let fb = new FormBuilder();
+
+    this.signupForm = fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.error = '';
+  }
+
+  signup(credentials) {
+    if (this.signupForm.valid) {
+      Accounts.createUser({ email: credentials.email, password: credentials.password}, (err) => {
+        if (err) {
+          this.error = err;
+        }
+        else {
+          this.router.navigate(['/PartiesList']);
+        }
+      });
+    }
+  }
+}
