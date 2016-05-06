412c52e35d700cb5f528f72eebd1947546fef671
dotansimha <dotansimha@gmail.com>
2016-05-06 21:48:19 +0300

    Step 8.7: check access to change party

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -2,6 +2,7 @@ import { Component, NgZone } from '@angular/core';
 import { RouteParams, RouterLink } from '@angular/router-deprecated';
 import { Tracker } from 'meteor/tracker';
 import {Parties} from '../../../collections/parties.ts';
+import { Meteor } from 'meteor/meteor';
 
 @Component({
   selector: 'party-details',
@@ -22,12 +23,16 @@ export class PartyDetails {
   }
 
   saveParty(party) {
-    Parties.update(party._id, {
-      $set: {
-        name: party.name,
-        description: party.description,
-        location: party.location
-      }
-    });
+    if (Meteor.userId()) {
+      Parties.update(party._id, {
+        $set: {
+          name: party.name,
+          description: party.description,
+          location: party.location
+        }
+      });
+    } else {
+      alert('Please log in to change this party');
+    }
   }
 }
