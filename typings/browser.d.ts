989cb7d7bd0b8449b08792e11c1c1cab6d833958
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-05-13 22:03:21 +0300

    Step 0.5: Added Typings dependencies

---
+++ typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/meteor/index.d.ts" />
+/// <reference path="browser/definitions/es6-promise/index.d.ts" />
