a04b3fbc15e91ca4617631c14b45f201cf97768e
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-05-14 13:39:12 +0300

    Step 4.1: create PartiesForm component

---
+++ client/imports/parties-form/parties-form.ts
@@ -0,0 +1,8 @@
+import 'reflect-metadata';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'parties-form',
+  templateUrl: '/client/imports/parties-form/parties-form.html'
+})
+export class PartiesForm { }
