e093869d595acdc3f9ae1a12ab929799c157c72b
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-05-14 14:29:18 +0300

    Step 5.6: add PartyDetails component

---
+++ client/imports/party-details/party-details.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'party-details',
+  templateUrl: '/client/imports/party-details/party-details.html'
+})
+export class PartyDetails {}
