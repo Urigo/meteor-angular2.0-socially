feda8c2566941f6104ba7b0727fd24f910e84fdf
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-05-14 13:56:27 +0300

    Step 4.5: build a form model

--- client/imports/parties-form/parties-form.ts
+++ client/imports/parties-form/parties-form.ts
@@ -1,8 +1,21 @@
 import 'reflect-metadata';
 import { Component } from '@angular/core';
+import { FormBuilder, ControlGroup, Validators } from '@angular/common';
 
 @Component({
   selector: 'parties-form',
   templateUrl: '/client/imports/parties-form/parties-form.html'
 })
-export class PartiesForm { }
+export class PartiesForm {
+  partiesForm: ControlGroup;
+
+  constructor() {
+    let fb = new FormBuilder();
+
+    this.partiesForm = fb.group({
+      name: [''],
+      description: [''],
+      location: ['']
+    });
+  }
+}
