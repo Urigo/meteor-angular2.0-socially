a4a06b57691600eedc145327af727a967b8aba6e
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-05-14 14:24:46 +0300

    Step 5.2: create PartiesList component

---
+++ client/imports/parties-list/parties-list.ts
@@ -0,0 +1,21 @@
+import { Component }   from '@angular/core';
+import { Parties }     from '../../../collections/parties';
+import { PartiesForm } from '../parties-form/parties-form';
+import { Mongo }       from 'meteor/mongo';
+
+@Component({
+  selector: 'parties-list',
+  templateUrl: '/client/imports/parties-list/parties-list.html',
+  directives: [PartiesForm]
+})
+export class PartiesList {
+  parties: Mongo.Cursor<Object>;
+
+  constructor() {
+    this.parties = Parties.find();
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
+  }
+}
