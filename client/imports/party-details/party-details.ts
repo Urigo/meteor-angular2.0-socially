f1d190225f4d3a4a5afa4f9ec1fb3a21ed856b1c
Uri Goldshtein <uri.goldshtein@gmail.com>
2016-05-14 14:51:47 +0300

    Step 5.12: inject route parameters

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { RouteParams } from '@angular/router-deprecated';
 
 @Component({
   selector: 'party-details',
   templateUrl: '/client/imports/party-details/party-details.html'
 })
-export class PartyDetails {}
+export class PartyDetails {
+  constructor(params: RouteParams) {
+    var partyId = params.get('partyId');
+  }
+}
