b9ef94fc412027e17f437b3f9d6c2cbce8d4c340
dotansimha <dotansimha@gmail.com>
2016-05-24 21:35:46 +0300

    Step 18.6: Import the components into the parties form

--- client/imports/parties-form/parties-form.ts
+++ client/imports/parties-form/parties-form.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 import { FormBuilder, ControlGroup, Validators, Control } from '@angular/common';
 import { Parties } from '../../../collections/parties.ts';
 import { Meteor } from 'meteor/meteor';
+import {MATERIAL_DIRECTIVES} from 'ng2-material';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
 
 import template from './parties-form.html';
 
 @Component({
   selector: 'parties-form',
-  template
+  template,
+  directives: [MATERIAL_DIRECTIVES, MdCheckbox, MD_INPUT_DIRECTIVES]
 })
 export class PartiesForm {
   partiesForm: ControlGroup;
