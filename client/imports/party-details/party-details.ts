5467c2830855fd165c113e6cf4c449f5bd934892
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-01 19:12:32 +0200

    Step 5.15: Run with NgZone

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Tracker } from 'meteor/tracker';
 import {Parties} from '../../../collections/parties.ts';
@@ -13,14 +13,16 @@ export class PartyDetails {
   partyId: string;
   party: Object;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private ngZone: NgZone) {}
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.partyId = params['partyId'];
 
       Tracker.autorun(() => {
-        this.party = Parties.findOne(this.partyId);
+        this.ngZone.run(() => {
+          this.party = Parties.findOne(this.partyId);
+        });
       });
     });
   }
