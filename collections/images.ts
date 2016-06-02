acfb80174e1132d651ffabca2fec2edd4704e32e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-02 19:04:16 +0200

    Step 20.8: Resize images

--- collections/images.ts
+++ collections/images.ts
@@ -23,7 +23,19 @@ Images.allow({
 
 export const ThumbsStore = new UploadFS.store.GridFS({
   collection: Thumbs,
-  name: 'thumbs'
+  name: 'thumbs',
+  transformWrite(from, to, fileId, file) {
+    // Resize to 32x32
+    const gm = require('gm');
+
+    gm(from, file.name)
+      .resize(32, 32)
+      .gravity('Center')
+      .extent(32, 32)
+      .quality(75)
+      .stream()
+      .pipe(to);
+  }
 });
 
 export const ImagesStore = new UploadFS.store.GridFS({
