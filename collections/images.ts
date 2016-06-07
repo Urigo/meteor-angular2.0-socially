03c1805d44bb445c9e0d0bcc5707d253b35f6f49
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-07 21:39:34 +0200

    Step 20.6: Add Image interface to Images Collection

--- collections/images.ts
+++ collections/images.ts
@@ -1,8 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
-export const Images = new Mongo.Collection('images');
-export const Thumbs = new Mongo.Collection('thumbs');
+export const Images = new Mongo.Collection<Image>('images');
+export const Thumbs = new Mongo.Collection<Thumb>('thumbs');
 
 function loggedIn(userId) {
   return !!userId;
