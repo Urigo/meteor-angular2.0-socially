72f8627a594954aa6adfeef3bf2f1925e5178caf
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-07 21:36:23 +0200

    Step 20.35: Create DisplayMainImage pipe

--- client/imports/pipes/pipes.ts
+++ client/imports/pipes/pipes.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { Pipe } from '@angular/core';
 import { Meteor } from 'meteor/meteor';
 import { Parties } from '../../../collections/parties.ts';
+import { Images } from '../../../collections/images';
 import { MeteorComponent } from 'angular2-meteor';
 
 @Pipe({
@@ -22,6 +23,34 @@ export class DisplayName {
     }
 
     return '';
+  }
+}
+
+@Pipe({
+  name: 'displayMainImage',
+  pure: false
+})
+export class DisplayMainImagePipe extends MeteorComponent {
+  constructor() {
+    super();
+  }
+
+  transform(party: Party) {
+    if (!party) {
+      return;
+    }
+
+    let imageUrl: string;
+    let imageId: string = (party.images || [])[0];
+
+    this.autorun(() => {
+      const found = Images.findOne(imageId);
+      if (found) {
+        imageUrl = found.url;
+      }
+    }, true);
+
+    return imageUrl;
   }
 }
 
