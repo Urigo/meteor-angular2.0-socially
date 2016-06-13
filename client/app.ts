2d13b4c8a28fca5baf6b71880bbeac5e4a18e04e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 16:52:06 +0200

    Step 21.11: Use mobile or web version of Login, depends on Meteor.isCordova

--- client/app.ts
+++ client/app.ts
@@ -16,7 +16,8 @@ import '../node_modules/@angular2-material/toolbar/toolbar.css';
 import { RouterLink } from '@angular/router-deprecated';
 import { DisplayName } from './imports/pipes/pipes';
 import { InjectUser } from 'angular2-meteor-accounts-ui';
-import { Login } from './imports/auth/login';
+import { Login as LoginWeb } from './imports/auth/login.web';
+import { Login as LoginMobile } from './imports/auth/login.mobile';
 import { Signup } from './imports/auth/signup';
 import { Recover } from './imports/auth/recover';
 import { Meteor } from 'meteor/meteor';
@@ -32,7 +33,7 @@ import { Meteor } from 'meteor/meteor';
 @RouteConfig([
   { path: '/', as: 'PartiesList', component: PartiesList },
   { path: '/party/:partyId', as: 'PartyDetails', component: PartyDetails },
-  { path: '/login', as: 'Login', component: Login },
+  { path: '/login', as: 'Login', component: Meteor.isCordova ? LoginMobile : LoginWeb },
   { path: '/signup', as: 'Signup', component: Signup },
   { path: '/recover', as: 'Recover', component: Recover }
 ])
