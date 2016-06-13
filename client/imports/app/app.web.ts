64073cae71fdd13a7e252c94fddb3f05ab2e5159
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 17:07:51 +0200

    Step 22.9: Update relative paths

--- client/imports/app/app.web.ts
+++ client/imports/app/app.web.ts
@@ -7,18 +7,18 @@ import { Component, provide } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { ROUTER_PROVIDERS, ROUTER_DIRECTIVES, RouteConfig } from '@angular/router-deprecated';
 import { APP_BASE_HREF } from '@angular/common';
-import { PartiesList } from './imports/parties-list/parties-list.ts';
-import { PartyDetails } from './imports/party-details/party-details.ts';
-import '../collections/methods.ts';
+import { PartiesList } from './..//parties-list/parties-list.ts';
+import { PartyDetails } from './..//party-details/party-details.ts';
+import '../../../collections/methods.ts';
 import {ANGULAR2_GOOGLE_MAPS_PROVIDERS} from 'angular2-google-maps/core';
 import {LoginButtons} from 'angular2-meteor-accounts-ui';
-import '../node_modules/@angular2-material/toolbar/toolbar.css';
+import '../../../node_modules/@angular2-material/toolbar/toolbar.css';
 import { RouterLink } from '@angular/router-deprecated';
-import { DisplayName } from './imports/pipes/pipes';
+import { DisplayName } from './..//pipes/pipes';
 import { InjectUser } from 'angular2-meteor-accounts-ui';
-import { Login } from './imports/auth/login.web';
-import { Signup } from './imports/auth/signup';
-import { Recover } from './imports/auth/recover';
+import { Login } from './..//auth/login.web';
+import { Signup } from './..//auth/signup';
+import { Recover } from './..//auth/recover';
 import { Meteor } from 'meteor/meteor';
 
 
