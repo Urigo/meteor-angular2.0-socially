0eb129eb4098de9cf9e1c2b5493910bf227a4e60
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 17:53:12 +0200

    Step 22.34: Set PartiesList as root page

--- client/imports/app/app.mobile.ts
+++ client/imports/app/app.mobile.ts
@@ -3,12 +3,16 @@ import { ionicBootstrap, App, Platform, MenuController } from 'ionic-angular';
 import { METEOR_PROVIDERS } from 'angular2-meteor';
 
 import template from './app.mobile.html';
+import { PartiesList } from '../parties-list/parties-list.mobile';
 
 @Component({
   template
 })
 class Socially {
-  constructor(app: App, platform: Platform, menu: MenuController) {}
+  rootPage: any;
+  constructor(app: App, platform: Platform, menu: MenuController) {
+    this.rootPage = PartiesList;
+  }
 }
 
 export function run(): void {
