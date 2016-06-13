1b4e4b7a3b6819202cee6cee110ea31449e2eba3
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 17:22:53 +0200

    Step 22.22: Use ionicSelector to change app into ion-app

--- client/main.ts
+++ client/main.ts
@@ -1,10 +1,12 @@
 import 'reflect-metadata';
+import ionicSelector from 'ionic-selector';
 import { Meteor } from 'meteor/meteor';
 import { run as runWeb } from './imports/app/app.web';
 import { run as runMobile } from './imports/app/app.mobile';
 
 if (Meteor.isCordova) {
   document.addEventListener('deviceready', () => {
+    ionicSelector('app');
     runMobile();
     setClass('mobile');
   });
