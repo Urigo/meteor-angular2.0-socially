f24ec73720f1e9eeffc828c03b2c4891637ee867
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 17:49:32 +0200

    Step 22.32: Import RsvpPipe

--- client/imports/parties-list/parties-list.mobile.ts
+++ client/imports/parties-list/parties-list.mobile.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { PartiesList as PartiesListBase } from './parties-list.class';
+import { RsvpPipe } from '../pipes/pipes';
 
 @Component({
-  templateUrl: '/client/imports/parties-list/parties-list.mobile.html'
+  templateUrl: '/client/imports/parties-list/parties-list.mobile.html',
+  pipes: [
+    RsvpPipe
+  ]
 })
 export class PartiesList extends PartiesListBase {
   constructor() {
