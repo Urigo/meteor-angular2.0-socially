cb8bfc984ab02798bb4914d2f4f10ad7997d0a1a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 17:16:26 +0200

    Step 22.17: Ionic version of Socially component

--- client/imports/app/app.mobile.ts
+++ client/imports/app/app.mobile.ts
@@ -1,3 +1,15 @@
+import { Component } from '@angular/core';
+import { App, Platform, MenuController } from 'ionic-angular';
+
+import template from './app.mobile.html';
+
+@Component({
+  template
+})
+class Socially {
+  constructor(app: App, platform: Platform, menu: MenuController) {}
+}
+
 export function run(): void {
   console.log('Mobile version is not yet available!');
 }
