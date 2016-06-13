27e9c729c8ddbb5702b1ccf5b251905a1e1cf42e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 18:03:04 +0200

    Step 22.36: Add DisplayMainImagePipe to PartiesList

--- client/imports/parties-list/parties-list.mobile.ts
+++ client/imports/parties-list/parties-list.mobile.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { PartiesList as PartiesListBase } from './parties-list.class';
 import template from './parties-list.mobile.html';
-import { RsvpPipe } from '../pipes/pipes';
+import { RsvpPipe, DisplayMainImagePipe } from '../pipes/pipes';
 
 @Component({
   template,
   pipes: [
-    RsvpPipe
+    RsvpPipe,
+    DisplayMainImagePipe
   ]
 })
 export class PartiesList extends PartiesListBase {
