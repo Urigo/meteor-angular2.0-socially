682a6bbde66421f8b122b035bd5fc90acc5c5f87
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-06-13 16:41:17 +0200

    Step 21.8: Implement phone verification

--- client/imports/auth/login.mobile.ts
+++ client/imports/auth/login.mobile.ts
@@ -5,6 +5,7 @@ import { MATERIAL_DIRECTIVES } from 'ng2-material';
 import { Router , ROUTER_DIRECTIVES} from '@angular/router';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 import { MdToolbar } from '@angular2-material/toolbar';
+import { Accounts } from 'meteor/accounts-base';
 
 import template from './login.mobile.html';
 
@@ -14,11 +15,30 @@ import template from './login.mobile.html';
   template
 })
 export class Login extends MeteorComponent {
+  phoneForm: ControlGroup;
+  phone: string;
   error: string;
 
   constructor(private router: Router) {
     super();
 
+    this.phoneForm = new FormBuilder().group({
+      phone: ['', Validators.required]
+    });
+
     this.error = '';
+  }
+
+  send(credentials) {
+    if (this.phoneForm.valid) {
+      Accounts.requestPhoneVerification(credentials.phone, (err) => {
+        if (err) {
+          this.error = err.reason || err;
+        } else {
+          this.error = '';
+          this.phone = credentials.phone;
+        }
+      });
+    }
   }
 }
