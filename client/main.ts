111576e47dc2dcb57bcf8c7b14b94f934f43cfdd
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 15:22:47 +0200

    Step 5.6: Use routes

--- client/main.ts
+++ client/main.ts
@@ -2,8 +2,10 @@ import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
-  provideForms()
+  provideForms(),
+  APP_ROUTER_PROVIDERS
 ]);
