c01b2cae86792bae221ffce1940374722f20506e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 15:15:09 +0200

    Step 5.3: Clean up App component

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,24 +1,9 @@
 import { Component } from '@angular/core';
-import { Mongo } from 'meteor/mongo';
-
-import { Parties }   from '../both/collections/parties.collection';
-import { PartiesFormComponent } from './imports/parties/parties-form.component';
 
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
-  template,
-  directives: [PartiesFormComponent]
+  template
 })
-export class AppComponent {
-  parties: Mongo.Cursor<any>;
-
-  constructor() {
-    this.parties = Parties.find();
-  }
-
-  removeParty(party) {
-    Parties.remove(party._id);
-  }
-}
+export class AppComponent {}
