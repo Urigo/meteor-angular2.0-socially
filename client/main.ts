bd15bcc5aaaecda33e4c7d2b8bb264e13c47090e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 12:36:34 +0200

    Step 4.5: Form Providers

--- client/main.ts
+++ client/main.ts
@@ -1,5 +1,9 @@
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  disableDeprecatedForms(),
+  provideForms()
+]);
