a3d50aee56a797df4ebde8d23614f4b0d6083516
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 14:28:25 +0200

    Step 4.8: Add validators

--- client/imports/parties/parties-form.component.ts
+++ client/imports/parties/parties-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder } from '@angular/forms';
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import template from './parties-form.component.html';
 
@@ -15,9 +15,9 @@ export class PartiesFormComponent implements OnInit {
 
   ngOnInit() {
     this.addForm = this.formBuilder.group({
-      name: [],
+      name: ['', Validators.required],
       description: [],
-      location: []
+      location: ['', Validators.required]
     });
   }
 }
