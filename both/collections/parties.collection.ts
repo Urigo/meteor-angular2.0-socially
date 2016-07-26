e6a2520f78d6c96120f2754490535f9347c255d1
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 17:14:13 +0200

    Step 8.5: Add Parties collection security

--- both/collections/parties.collection.ts
+++ both/collections/parties.collection.ts
@@ -3,3 +3,13 @@ import {Mongo} from 'meteor/mongo';
 import {Party} from '../interfaces/party.interface';
 
 export const Parties = new Mongo.Collection<Party>('parties');
+
+function loggedIn() {
+  return !!Meteor.user();
+}
+
+Parties.allow({
+  insert: loggedIn,
+  update: loggedIn,
+  remove: loggedIn
+});
