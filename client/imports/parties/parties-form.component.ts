913158998b2f9ca9685ece6b40939e812ce0a30c
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 12:37:11 +0200

    Step 4.6: Create form

--- client/imports/parties/parties-form.component.ts
+++ client/imports/parties/parties-form.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder } from '@angular/forms';
 
 import template from './parties-form.component.html';
 
 @Component({
   selector: 'parties-form',
-  template
+  template,
+  directives: [REACTIVE_FORM_DIRECTIVES]
 })
-export class PartiesFormComponent { }
+export class PartiesFormComponent implements OnInit {
+  addForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      name: [],
+      description: [],
+      location: []
+    });
+  }
+}
