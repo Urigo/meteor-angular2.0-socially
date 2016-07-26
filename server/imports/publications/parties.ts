ea8c765b04583004fb5f492845c063c07b232c9a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 18:48:04 +0200

    Step 9.9: Limit data sent to the client

--- server/imports/publications/parties.ts
+++ server/imports/publications/parties.ts
@@ -1,4 +1,18 @@
 import {Parties} from '../../../both/collections/parties.collection';
 import {Meteor} from 'meteor/meteor';
 
-Meteor.publish('parties', () => Parties.find());
+Meteor.publish('parties', function() {
+  const selector = {
+    $or: [
+      { 'public': true },
+      {
+        $and: [
+          { owner: this.userId },
+          { owner: { $exists: true } }
+        ]
+      }
+    ]
+  };
+
+  return Parties.find(selector);
+});
