984538a6d39d2b08658d5d45c98b9d8e6a88340e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 15:42:50 +0200

    Step 5.11: Add route for PartyDetails

--- client/app.routes.ts
+++ client/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterConfig, provideRouter } from '@angular/router';
 
 import { PartiesListComponent } from './imports/parties/parties-list.component';
+import { PartyDetailsComponent } from './imports/parties/party-details.component';
 
 const routes: RouterConfig = [
-  { path: '', component: PartiesListComponent }
+  { path: '', component: PartiesListComponent },
+  { path: 'party/:partyId', component: PartyDetailsComponent }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
