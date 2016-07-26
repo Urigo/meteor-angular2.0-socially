9ea4d7ae1a909f8e8b288781398e4ef93bcf8a37
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 14:58:28 +0200

    Step 4.10: Add addParty method

--- client/imports/parties/parties-form.component.ts
+++ client/imports/parties/parties-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { Parties } from '../../../both/collections/parties.collection';
+
 import template from './parties-form.component.html';
 
 @Component({
@@ -19,5 +21,20 @@ export class PartiesFormComponent implements OnInit {
       description: [],
       location: ['', Validators.required]
     });
+  }
+
+  resetForm() {
+    this.addForm.controls['name']['updateValue']('');
+    this.addForm.controls['description']['updateValue']('');
+    this.addForm.controls['location']['updateValue']('');
+  }
+
+  addParty() {
+    if (this.addForm.valid) {
+      Parties.insert(this.addForm.value);
+
+      // XXX will be replaced by this.addForm.reset() in RC5+
+      this.resetForm();
+    }
   }
 }
