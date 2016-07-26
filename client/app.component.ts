0639ca9ad2ae43f14b9185df44734a3cf29446ef
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-26 15:23:41 +0200

    Step 5.8: Add ROUTER_DIRECTIVES

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
-  template
+  template,
+  directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent {}
