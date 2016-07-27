da8cca6a6f85b78924f3fedff85ec2b81f2ea5fd
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-27 17:35:03 +0200

    Step 12.15: Add typigns for counts package

---
+++ typings/publish-counts.d.ts
@@ -0,0 +1,8 @@
+interface CountsObject {
+  get(publicationName: string): number;
+  publish(context: any, publicationName: string, cursor: Mongo.Cursor, options: any): number;
+}
+
+declare module "meteor/tmeasday:publish-counts" {
+  export let Counts: CountsObject;
+}
