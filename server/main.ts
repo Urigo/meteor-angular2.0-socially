7527200178caf707ff6cf4d4c3e20d7b28b9892a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-27 18:02:40 +0200

    Step 13.2: Add users publication to the server

--- server/main.ts
+++ server/main.ts
@@ -2,6 +2,7 @@ import {loadParties} from './imports/fixtures/parties';
 import {Meteor} from 'meteor/meteor';
 
 import './imports/publications/parties';
+import './imports/publications/users';
 
 Meteor.startup(() => {
   // load initial Parties
