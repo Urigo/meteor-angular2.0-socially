606c3e1a7dab3862212ba755437b83cf720e826b
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 16:13:24 +0200

    Step 18.15: Create LoginComponent

---
+++ client/imports/auth/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { MeteorComponent } from 'angular2-meteor';
+import { FormBuilder, FormGroup, Validators, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
+import { Router , ROUTER_DIRECTIVES} from '@angular/router';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { Meteor } from 'meteor/meteor';
+
+import template from './login.component.html';
+
+@Component({
+  selector: 'login',
+  template,
+  directives: [ROUTER_DIRECTIVES, MD_INPUT_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MD_BUTTON_DIRECTIVES, REACTIVE_FORM_DIRECTIVES],
+})
+export class LoginComponent extends MeteorComponent implements OnInit {
+  loginForm: FormGroup;
+  error: string;
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    super();
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.error = '';
+  }
+
+  login(credentials) {
+    if (this.loginForm.valid) {
+      Meteor.loginWithPassword(this.loginForm.value.email, this.loginForm.value.password, (err) => {
+        if (err) {
+          this.error = err;
+        } else {
+          this.router.navigate(['/']);
+        }
+      });
+    }
+  }
+}
