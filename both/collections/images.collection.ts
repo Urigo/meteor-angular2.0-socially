8f544b302ba186cc4438840ebe798690f41d361e
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 18:22:49 +0200

    Step 20.8: Resize images

--- both/collections/images.collection.ts
+++ both/collections/images.collection.ts
@@ -9,7 +9,19 @@ export const Thumbs = new Mongo.Collection<Thumb>('thumbs');
 
 export const ThumbsStore = new UploadFS.store.GridFS({
   collection: Thumbs,
-  name: 'thumbs'
+  name: 'thumbs',
+  transformWrite(from, to, fileId, file) {
+    // Resize to 32x32
+    const gm = require('gm');
+
+    gm(from, file.name)
+      .resize(32, 32)
+      .gravity('Center')
+      .extent(32, 32)
+      .quality(75)
+      .stream()
+      .pipe(to);
+  }
 });
 
 export const ImagesStore = new UploadFS.store.GridFS({
