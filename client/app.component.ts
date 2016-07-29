3abc567b1734f7435294c2bf2254f3db99bfcb9d
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 12:48:11 +0200

    Step 17.7: Import LoginButtons

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { LoginButtons } from 'angular2-meteor-accounts-ui';
 
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
   template,
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, LoginButtons]
 })
 export class AppComponent {}
