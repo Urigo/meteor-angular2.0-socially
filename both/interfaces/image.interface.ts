c0c3bea9cbcdafdfa48a3962f2815e6a68d48339
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 18:11:29 +0200

    Step 20.4: Define Image interface

---
+++ both/interfaces/image.interface.ts
@@ -0,0 +1,15 @@
+export interface Image {
+  _id?: string;
+  complete: boolean;
+  extension: string;
+  name: string;
+  progress: number;
+  size: number;
+  store: string;
+  token: string;
+  type: string;
+  uploadedAt: Date;
+  uploading: boolean;
+  url: string;
+  userId?: string;
+}
