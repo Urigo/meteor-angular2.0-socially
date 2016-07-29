e2eda4f54fe63fc3ab17388749764baf37ef83a8
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 18:21:19 +0200

    Step 20.7: Create stores for Images and Thumbs

--- both/collections/images.collection.ts
+++ both/collections/images.collection.ts
@@ -1,10 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { UploadFS } from 'meteor/jalik:ufs';
 
 import { Image, Thumb } from '../interfaces/image.interface';
 
 export const Images = new Mongo.Collection<Image>('images');
 export const Thumbs = new Mongo.Collection<Thumb>('thumbs');
+
+export const ThumbsStore = new UploadFS.store.GridFS({
+  collection: Thumbs,
+  name: 'thumbs'
+});
+
+export const ImagesStore = new UploadFS.store.GridFS({
+  collection: Images,
+  name: 'images',
+  filter: new UploadFS.Filter({
+    contentTypes: ['image/*']
+  }),
+  copyTo: [
+    ThumbsStore
+  ]
+});
 
 function loggedIn(userId) {
   return !!userId;
