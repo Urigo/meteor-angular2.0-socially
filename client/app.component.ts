6023d6c2f21fbbefc099ff8f30451ae6e2681dfb
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 13:57:14 +0200

    Step 18.4: Import toolbar in the App

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { LoginButtons } from 'angular2-meteor-accounts-ui';
+import { MdToolbar } from '@angular2-material/toolbar';
 
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
   template,
-  directives: [ROUTER_DIRECTIVES, LoginButtons]
+  directives: [ROUTER_DIRECTIVES, LoginButtons, MdToolbar]
 })
 export class AppComponent {}
