d2192b6c6f81dc5d103e3ae29978494b1982aede
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 16:07:26 +0200

    Step 18.14: Add some logic to the App component

--- client/app.component.ts
+++ client/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { LoginButtons } from 'angular2-meteor-accounts-ui';
+import { LoginButtons, InjectUser } from 'angular2-meteor-accounts-ui';
+import { MeteorComponent } from 'angular2-meteor';
 import { MdToolbar } from '@angular2-material/toolbar';
+import { Meteor } from 'meteor/meteor';
+
+import { DisplayNamePipe } from './imports/shared/display-name.pipe';
 
 import template from './app.component.html';
 
 @Component({
   selector: 'app',
   template,
-  directives: [ROUTER_DIRECTIVES, LoginButtons, MdToolbar]
+  directives: [ROUTER_DIRECTIVES, LoginButtons, MdToolbar],
+  pipes: [DisplayNamePipe]
 })
-export class AppComponent {}
+@InjectUser('user')
+export class AppComponent extends MeteorComponent {
+  user: Meteor.User;
+
+  constructor() {
+    super();
+  }
+
+  logout() {
+    Meteor.logout();
+  }
+}
