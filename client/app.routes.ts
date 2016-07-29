96062f4f6aaf7f0753b0e6c1d169fb9eb3ce00b2
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-29 17:36:38 +0200

    Step 18.23: Define the reset route

--- client/app.routes.ts
+++ client/app.routes.ts
@@ -5,12 +5,14 @@ import { PartiesListComponent } from './imports/parties/parties-list.component';
 import { PartyDetailsComponent } from './imports/parties/party-details.component';
 import { LoginComponent } from './imports/auth/login.component';
 import { SignupComponent } from './imports/auth/signup.component';
+import { RecoverComponent } from './imports/auth/recover.component';
 
 const routes: RouterConfig = [
   { path: '', component: PartiesListComponent },
   { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['CanActivateForLoggedIn'] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'recover', component: RecoverComponent },
 ];
 
 export const APP_ROUTER_PROVIDERS = [
