0499e77fc3b09b4a643859bf070b5ea8663bbbca
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 16:07:56 +0200

    Step 22.8: Use Meteor.isCordova

--- client/main.ts
+++ client/main.ts
@@ -1,11 +1,18 @@
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { GOOGLE_MAPS_PROVIDERS } from 'angular2-google-maps/core';
+import { Meteor } from 'meteor/meteor';
 
 import { AppComponent } from './app.web.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 import '../both/methods/parties.methods';
+
+if (Meteor.isCordova) {
+  runMobile();
+} else {
+  runWeb();
+}
 
 function runWeb() {
   bootstrap(AppComponent, [
