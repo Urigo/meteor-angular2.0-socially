4d6276607701be7b758573c234c5c3cc316d18cf
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 17:39:35 +0200

    Step 22.37: Import RsvpPipe

--- client/imports/parties/parties-list.mobile.component.ts
+++ client/imports/parties/parties-list.mobile.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { PaginationService, PaginationControlsCmp } from 'ng2-pagination';
 
 import { PartiesList } from './parties-list.class';
+import { RsvpPipe } from '../shared/rsvp.pipe';
 
 import template from './parties-list.mobile.component.html';
 
 @Component({
   template,
   viewProviders: [PaginationService],
-  directives: [PaginationControlsCmp]
+  directives: [PaginationControlsCmp],
+  pipes: [RsvpPipe]
 })
 export class PartiesListComponent extends PartiesList {
   constructor(paginationService: PaginationService) {
