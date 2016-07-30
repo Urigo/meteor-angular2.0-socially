12a9e37fba7269bd54e77ff1c5f8833ae23b22d0
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 17:33:20 +0200

    Step 22.31: Use only needed things

--- client/imports/parties/parties-list.web.component.ts
+++ client/imports/parties/parties-list.web.component.ts
@@ -1,21 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-import { Mongo } from 'meteor/mongo';
-import { ReactiveVar } from 'meteor/reactive-var';
-import { Counts } from 'meteor/tmeasday:publish-counts';
-import { InjectUser } from 'angular2-meteor-accounts-ui';
-import { MeteorComponent } from 'angular2-meteor';
 import { PaginationService, PaginationControlsCmp } from 'ng2-pagination';
 import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
 import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
 import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
 
-import { Parties }   from '../../../both/collections/parties.collection';
-import { Party } from '../../../both/interfaces/party.interface';
 import { PartiesFormComponent } from './parties-form.component';
 import { RsvpPipe } from '../shared/rsvp.pipe';
 import { DisplayMainImagePipe } from '../shared/display-main-image.pipe';
+import { PartiesList } from './parties-list.class';
 
 import template from './parties-list.component.html';
 
@@ -26,72 +20,8 @@ import template from './parties-list.component.html';
   directives: [MD_CARD_DIRECTIVES, MD_TOOLBAR_DIRECTIVES, MD_INPUT_DIRECTIVES, GOOGLE_MAPS_DIRECTIVES, PartiesFormComponent, ROUTER_DIRECTIVES, PaginationControlsCmp],
   pipes: [RsvpPipe, DisplayMainImagePipe]
 })
-@InjectUser('user')
-export class PartiesListComponent extends MeteorComponent implements OnInit {
-  parties: Mongo.Cursor<Party>;
-  partiesSize: number = 0;
-  pageSize: number = 10;
-  curPage: ReactiveVar<number> = new ReactiveVar<number>(1);
-  nameOrder: ReactiveVar<number> = new ReactiveVar<number>(1);
-  location: ReactiveVar<string> = new ReactiveVar<string>(null);
-  loading: boolean = false;
-  user: Meteor.User;
-
-  constructor(private paginationService: PaginationService) {
-    super();
-  }
-
-  ngOnInit() {
-    this.paginationService.register({
-      id: this.paginationService.defaultId,
-      itemsPerPage: this.pageSize,
-      currentPage: this.curPage.get(),
-      totalItems: this.partiesSize,
-    });
-
-    this.autorun(() => {
-      const options = {
-        limit: this.pageSize,
-        skip: (this.curPage.get() - 1) * this.pageSize,
-        sort: { name: this.nameOrder.get() }
-      };
-
-      this.loading = true;
-      this.paginationService.setCurrentPage(this.paginationService.defaultId, this.curPage.get());
-
-      this.subscribe('parties', options, this.location.get(), () => {
-        this.parties = Parties.find({}, {sort: { name: this.nameOrder.get() }});
-        this.loading = false;
-      }, true);
-    });
-
-
-    this.autorun(() => {
-      this.partiesSize = Counts.get('numberOfParties');
-      this.paginationService.setTotalItems(this.paginationService.defaultId, this.partiesSize);
-    });
-
-    this.subscribe('images');
-  }
-
-  removeParty(party) {
-    Parties.remove(party._id);
-  }
-
-  search(value: string) {
-    this.curPage.set(1);
-    this.location.set(value);
-  }
-
-  changeSortOrder(nameOrder: string) {
-    this.nameOrder.set(parseInt(nameOrder));
-  }
-
-  onPageChanged(page: number) {
-    this.curPage.set(page);
-  }
-
-  isOwner(party: Party): boolean {
-    return this.user && this.user._id === party.owner;
+export class PartiesListComponent extends PartiesList {
+  constructor(paginationService: PaginationService) {
+    super(paginationService);
   }
 }
