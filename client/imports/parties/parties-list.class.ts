3b0120f8dd9b9575dcccd1299c86431c3c473c06
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 17:25:49 +0200

    Step 22.26: Move the PartiesListComponent class to a separate file

---
+++ client/imports/parties/parties-list.class.ts
@@ -0,0 +1,68 @@
+export class PartiesListComponent extends MeteorComponent implements OnInit {
+  parties: Mongo.Cursor<Party>;
+  partiesSize: number = 0;
+  pageSize: number = 10;
+  curPage: ReactiveVar<number> = new ReactiveVar<number>(1);
+  nameOrder: ReactiveVar<number> = new ReactiveVar<number>(1);
+  location: ReactiveVar<string> = new ReactiveVar<string>(null);
+  loading: boolean = false;
+  user: Meteor.User;
+
+  constructor(private paginationService: PaginationService) {
+    super();
+  }
+
+  ngOnInit() {
+    this.paginationService.register({
+      id: this.paginationService.defaultId,
+      itemsPerPage: this.pageSize,
+      currentPage: this.curPage.get(),
+      totalItems: this.partiesSize,
+    });
+
+    this.autorun(() => {
+      const options = {
+        limit: this.pageSize,
+        skip: (this.curPage.get() - 1) * this.pageSize,
+        sort: { name: this.nameOrder.get() }
+      };
+
+      this.loading = true;
+      this.paginationService.setCurrentPage(this.paginationService.defaultId, this.curPage.get());
+
+      this.subscribe('parties', options, this.location.get(), () => {
+        this.parties = Parties.find({}, {sort: { name: this.nameOrder.get() }});
+        this.loading = false;
+      }, true);
+    });
+
+
+    this.autorun(() => {
+      this.partiesSize = Counts.get('numberOfParties');
+      this.paginationService.setTotalItems(this.paginationService.defaultId, this.partiesSize);
+    });
+
+    this.subscribe('images');
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
+  }
+
+  search(value: string) {
+    this.curPage.set(1);
+    this.location.set(value);
+  }
+
+  changeSortOrder(nameOrder: string) {
+    this.nameOrder.set(parseInt(nameOrder));
+  }
+
+  onPageChanged(page: number) {
+    this.curPage.set(page);
+  }
+
+  isOwner(party: Party): boolean {
+    return this.user && this.user._id === party.owner;
+  }
+}
