ad84d4e79eb467e84eb5a070f37b4369978a6b6d
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 17:41:32 +0200

    Step 22.39: Set PartiesListComponent as a root page

--- client/app.mobile.component.ts
+++ client/app.mobile.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { App, Platform, MenuController } from 'ionic-angular';
 
+import { PartiesListComponent } from './imports/parties/parties-list.mobile.component';
+
 import template from './app.mobile.component.html';
 
 @Component({
   template
 })
 export class AppComponent {
-  constructor(app: App, platform: Platform, menu: MenuController) {}
+  rootPage: any;
+
+  constructor(app: App, platform: Platform, menu: MenuController) {
+    this.rootPage = PartiesListComponent;
+  }
 }
