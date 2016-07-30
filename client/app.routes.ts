cd05997b4c2aeffb539005552cc16bf952f96c8b
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 15:56:04 +0200

    Step 21.12: Choose mobile or web version

--- client/app.routes.ts
+++ client/app.routes.ts
@@ -3,14 +3,15 @@ import { Meteor } from 'meteor/meteor';
 
 import { PartiesListComponent } from './imports/parties/parties-list.component';
 import { PartyDetailsComponent } from './imports/parties/party-details.component';
-import { LoginComponent } from './imports/auth/login.component';
+import { LoginComponent as LoginWebComponent } from './imports/auth/login.web.component';
+import { LoginComponent as LoginMobileComponent } from './imports/auth/login.mobile.component';
 import { SignupComponent } from './imports/auth/signup.component';
 import { RecoverComponent } from './imports/auth/recover.component';
 
 const routes: RouterConfig = [
   { path: '', component: PartiesListComponent },
   { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['CanActivateForLoggedIn'] },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: Meteor.isCordova ? LoginMobileComponent : LoginWebComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'recover', component: RecoverComponent },
 ];
