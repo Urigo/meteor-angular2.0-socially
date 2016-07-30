9b0c56dcdd2fbeb42030bbe0ab31bcd69f522e3a
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 10:42:54 +0200

    Step 20.21: Install definitions of NodeJS

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,4 +3,5 @@
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/meteor/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
 /// <reference path="modules/chai-spies/index.d.ts" />
