a8b0e86f0409cfe077b20ba2068486fc838c5028
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-30 16:04:40 +0200

    Step 22.6: Update the App component path in the main.ts

--- client/main.ts
+++ client/main.ts
@@ -2,7 +2,7 @@ import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { GOOGLE_MAPS_PROVIDERS } from 'angular2-google-maps/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.web.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 import '../both/methods/parties.methods';
