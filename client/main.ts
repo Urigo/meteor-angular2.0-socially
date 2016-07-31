334370a1c768ecd77d0d4b2795498d2090117c0b
Kamil Kisiela <kamil.kisiela@ster-project.pl>
2016-07-31 23:32:36 +0200

    Step 14.3: Import methods on the client side

--- client/main.ts
+++ client/main.ts
@@ -4,6 +4,8 @@ import { disableDeprecatedForms, provideForms } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { APP_ROUTER_PROVIDERS } from './app.routes';
 
+import '../both/methods/parties.methods';
+
 bootstrap(AppComponent, [
   disableDeprecatedForms(),
   provideForms(),
