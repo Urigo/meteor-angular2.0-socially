7acdbfae13287a35e9be4b57c740bf75d3ad2cf1
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 18:48:22 +0200

    Step 6.11: Add component to declarations

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Route } from '@angular/router';
 
 import { PartiesListComponent } from './parties/parties-list.component';
+import { PartyDetailsComponent } from './parties/party-details.component';
 
 export const routes: Route[] = [
-  { path: '', component: PartiesListComponent }
+  { path: '', component: PartiesListComponent },
+  { path: 'party/:partyId', component: PartyDetailsComponent }
 ];
