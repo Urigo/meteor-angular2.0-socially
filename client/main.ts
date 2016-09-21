efbb453f28765a3533e336aee3d41703a19cbba5
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 16:22:46 +0200

    Step 1.9: Create main entry point

---
+++ client/main.ts
@@ -0,0 +1,8 @@
+import 'angular2-meteor-polyfills';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './imports/app/app.module';
+
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
