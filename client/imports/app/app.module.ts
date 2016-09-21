4c879956cd838dc55a886a87c210823d367c5eca
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 20:41:37 +0200

    Step 9.2: Import AccountsModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AccountsModule } from 'angular2-meteor-accounts-ui';
 
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
@@ -12,7 +13,8 @@ import { PARTIES_DECLARATIONS } from './parties';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AccountsModule
   ],
   declarations: [
     AppComponent,
