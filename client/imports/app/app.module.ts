cccb2259de7da85457973e9b86ef96502789c286
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 18:13:23 +0200

    Step 5.5: Add parties declarations to AppModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PARTIES_DECLARATIONS } from './parties';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    ...PARTIES_DECLARATIONS
   ],
   bootstrap: [
     AppComponent
