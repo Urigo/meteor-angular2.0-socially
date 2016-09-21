af561f4e6b749373f823f1e0180027aae95565dc
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 21:37:13 +0200

    Step 9.10: Require user to access PartyDetails

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Route } from '@angular/router';
+import { Meteor } from 'meteor/meteor';
 
 import { PartiesListComponent } from './parties/parties-list.component';
 import { PartyDetailsComponent } from './parties/party-details.component';
 
 export const routes: Route[] = [
   { path: '', component: PartiesListComponent },
-  { path: 'party/:partyId', component: PartyDetailsComponent }
+  { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['canActivateForLoggedIn'] }
 ];
+
+export const ROUTES_PROVIDERS = [{
+  provide: 'canActivateForLoggedIn',
+  useValue: () => !! Meteor.userId()
+}];
