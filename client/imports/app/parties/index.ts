741de8c783d8be3ced8e2cc0d2bb72e88e13992f
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 18:48:22 +0200

    Step 6.11: Add component to declarations

--- client/imports/app/parties/index.ts
+++ client/imports/app/parties/index.ts
@@ -1,7 +1,9 @@
 import { PartiesFormComponent } from './parties-form.component';
 import { PartiesListComponent } from './parties-list.component';
+import { PartyDetailsComponent } from './party-details.component';
 
 export const PARTIES_DECLARATIONS = [
   PartiesFormComponent,
-  PartiesListComponent
+  PartiesListComponent,
+  PartyDetailsComponent
 ];
