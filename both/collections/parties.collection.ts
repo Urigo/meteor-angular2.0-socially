c1c02577d334cf0b68ad97701abc965f81ffb147
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 21:20:37 +0200

    Step 9.5: Add Parties collection security

--- both/collections/parties.collection.ts
+++ both/collections/parties.collection.ts
@@ -1,5 +1,16 @@
 import { MongoObservable } from 'meteor-rxjs';
+import { Meteor } from 'meteor/meteor';
 
 import { Party } from '../models/party.model';
 
 export const Parties = new MongoObservable.Collection<Party>('parties');
+
+function loggedIn() {
+  return !!Meteor.user();
+}
+
+Parties.allow({
+  insert: loggedIn,
+  update: loggedIn,
+  remove: loggedIn
+});
