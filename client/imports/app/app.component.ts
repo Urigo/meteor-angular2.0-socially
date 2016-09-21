ec5b1e4b961970265679d7b8a5f861506a03d17e
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 17:05:17 +0200

    Step 3.2: Load parties data into app

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -6,4 +6,23 @@ import template from './app.component.html';
   selector: 'app',
   template
 })
-export class AppComponent {}
+export class AppComponent {
+  parties: any[];
+
+  constructor() {
+    this.parties = [
+      {'name': 'Dubstep-Free Zone',
+        'description': 'Can we please just for an evening not listen to dubstep.',
+        'location': 'Palo Alto'
+      },
+      {'name': 'All dubstep all the time',
+        'description': 'Get it on!',
+        'location': 'Palo Alto'
+      },
+      {'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.',
+        'location': 'San Francisco'
+      }
+    ];
+  }
+}
