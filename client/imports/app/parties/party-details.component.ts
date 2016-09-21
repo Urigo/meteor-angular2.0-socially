1ca6fadecc3dfa5a1e60403c35f57758d16ff69e
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 18:54:26 +0200

    Step 6.14: Unsubscribe when component is being destroyed

--- client/imports/app/parties/party-details.component.ts
+++ client/imports/app/parties/party-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 import 'rxjs/add/operator/map';
 
@@ -9,16 +10,21 @@ import template from './party-details.component.html';
   selector: 'party-details',
   template
 })
-export class PartyDetailsComponent implements OnInit {
+export class PartyDetailsComponent implements OnInit, OnDestroy {
   partyId: string;
+  paramsSub: Subscription;
 
   constructor(
     private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.route.params
+    this.paramsSub = this.route.params
       .map(params => params['partyId'])
       .subscribe(partyId => this.partyId = partyId);
+  }
+
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
   }
 }
