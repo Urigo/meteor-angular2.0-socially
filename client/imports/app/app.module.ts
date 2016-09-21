d02611156c6b10e4bb0adadb936dfddcfddb1287
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 18:14:46 +0200

    Step 5.6: Import Forms modules

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PARTIES_DECLARATIONS } from './parties';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
