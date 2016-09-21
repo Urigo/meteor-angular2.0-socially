b5f58782ed8484ec211d0360f2e464c575d9f6e1
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 17:53:05 +0200

    Step 4.8: Add Party type to Parties collection

--- both/collections/parties.collection.ts
+++ both/collections/parties.collection.ts
@@ -1,3 +1,5 @@
 import { MongoObservable } from 'meteor-rxjs';
 
-export const Parties = new MongoObservable.Collection('parties');
+import { Party } from '../models/party.model';
+
+export const Parties = new MongoObservable.Collection<Party>('parties');
