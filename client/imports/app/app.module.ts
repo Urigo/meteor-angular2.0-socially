811d18c6022a746019ba74861a8aab9b5f1f328e
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 18:37:43 +0200

    Step 6.7: Register RouterModule

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { routes } from './app.routes';
 import { PARTIES_DECLARATIONS } from './parties';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   declarations: [
     AppComponent,
