a1b673c2b92eb20c93cf048e74379a034bf0817a
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-21 17:38:50 +0200

    Step 4.3: Bind MongoObservable to Angular

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 import { Parties } from '../../../both/collections/parties.collection';
 
@@ -9,22 +10,9 @@ import template from './app.component.html';
   template
 })
 export class AppComponent {
-  parties: any[];
+  parties: Observable<any[]>;
 
   constructor() {
-    this.parties = [
-      {'name': 'Dubstep-Free Zone',
-        'description': 'Can we please just for an evening not listen to dubstep.',
-        'location': 'Palo Alto'
-      },
-      {'name': 'All dubstep all the time',
-        'description': 'Get it on!',
-        'location': 'Palo Alto'
-      },
-      {'name': 'Savage lounging',
-        'description': 'Leisure suit required. And only fiercest manners.',
-        'location': 'San Francisco'
-      }
-    ];
+    this.parties = Parties.find({}).zone();
   }
 }
