5b98c1319131d76ab87b33acadd45a58397352a2
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-22 12:57:35 +0200

    Step 10.3: Add publication to server-side entry point

--- server/main.ts
+++ server/main.ts
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 import { loadParties } from './imports/fixtures/parties';
 
+import './imports/publications/parties'; 
+
 Meteor.startup(() => {
   loadParties();
 });
