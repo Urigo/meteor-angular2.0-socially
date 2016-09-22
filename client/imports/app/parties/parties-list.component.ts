31b254fa82d7c9ef6a335b7bbde593d3737ae827
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-22 14:37:54 +0200

    Step 10.13: Add search method

--- client/imports/app/parties/parties-list.component.ts
+++ client/imports/app/parties/parties-list.component.ts
@@ -25,6 +25,10 @@ export class PartiesListComponent implements OnInit, OnDestroy {
     Parties.remove(party._id);
   }
 
+  search(value: string): void {
+    this.parties = Parties.find(value ? { location: value } : {}).zone();
+  }
+
   ngOnDestroy() {
     this.partiesSub.unsubscribe();
   }
