d5a4b19c3d9d5e8f96266706146e97e7f32aee8a
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-22 14:05:26 +0200

    Step 10.7: Update parties initials

--- server/imports/fixtures/parties.ts
+++ server/imports/fixtures/parties.ts
@@ -6,15 +6,18 @@ export function loadParties() {
     const parties: Party[] = [{
       name: 'Dubstep-Free Zone',
       description: 'Can we please just for an evening not listen to dubstep.',
-      location: 'Palo Alto'
+      location: 'Palo Alto',
+      public: true
     }, {
       name: 'All dubstep all the time',
       description: 'Get it on!',
-      location: 'Palo Alto'
+      location: 'Palo Alto',
+      public: true
     }, {
       name: 'Savage lounging',
       description: 'Leisure suit required. And only fiercest manners.',
-      location: 'San Francisco'
+      location: 'San Francisco',
+      public: false
     }];
 
     parties.forEach((party: Party) => Parties.insert(party));
