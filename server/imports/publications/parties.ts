846baecde45ce1267cc5b452b138381b3bc39f66
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-22 14:09:53 +0200

    Step 10.9: Limit data sent to the client

--- server/imports/publications/parties.ts
+++ server/imports/publications/parties.ts
@@ -1,4 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { Parties } from '../../../both/collections/parties.collection';
 
-Meteor.publish('parties', () => Parties.find());
+Meteor.publish('parties', function() {
+  const selector = {
+    $or: [{
+      // party is public
+      public: true
+    },
+    // or
+    { 
+      // current user is the owner
+      $and: [{
+        owner: this.userId 
+      }, {
+        owner: {
+          $exists: true
+        }
+      }]
+    }]
+  };
+
+  return Parties.find(selector);
+});
