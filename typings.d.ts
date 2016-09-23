a16036e95f4d9eca752a81cc04a2a98c1505c469
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-23 13:15:19 +0200

    Step 13.15: Add declaration of counts package

--- typings.d.ts
+++ typings.d.ts
@@ -26,3 +26,14 @@ declare module '*.sass' {
   const style: string;
   export default style;
 }
+
+declare module 'meteor/tmeasday:publish-counts' {
+  import { Mongo } from 'meteor/mongo';
+
+  interface CountsObject {
+    get(publicationName: string): number;
+    publish(context: any, publicationName: string, cursor: Mongo.Cursor, options: any): number;
+  }
+
+  export const Counts: CountsObject;
+}
