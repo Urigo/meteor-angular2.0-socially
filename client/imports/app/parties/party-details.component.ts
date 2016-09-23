12521cbed81409177b90b5751933c947741c94e1
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-23 17:45:17 +0200

    Step 14.6: Implement subscription of uninvited users

--- client/imports/app/parties/party-details.component.ts
+++ client/imports/app/parties/party-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import { Meteor } from 'meteor/meteor';
 import { MeteorObservable } from 'meteor-rxjs';
@@ -8,6 +9,8 @@ import 'rxjs/add/operator/map';
 
 import { Parties } from '../../../../both/collections/parties.collection';
 import { Party } from '../../../../both/models/party.model';
+import { Users } from '../../../../both/collections/users.collection';
+import { User } from '../../../../both/models/user.model';
 
 import template from './party-details.component.html';
 
@@ -20,6 +23,8 @@ export class PartyDetailsComponent implements OnInit, OnDestroy {
   paramsSub: Subscription;
   party: Party;
   partySub: Subscription;
+  users: Observable<User>;
+  uninvitedSub: Subscription;
 
   constructor(
     private route: ActivatedRoute
@@ -37,6 +42,18 @@ export class PartyDetailsComponent implements OnInit, OnDestroy {
 
         this.partySub = MeteorObservable.subscribe('party', this.partyId).subscribe(() => {
           this.party = Parties.findOne(this.partyId);
+        });
+
+        if (this.uninvitedSub) {
+          this.uninvitedSub.unsubscribe();
+        }
+
+        this.uninvitedSub = MeteorObservable.subscribe('uninvited', this.partyId).subscribe(() => {
+           this.users = Users.find({
+             _id: {
+               $ne: Meteor.userId()
+              }
+            }).zone();
         });
       });
   }
@@ -59,5 +76,6 @@ export class PartyDetailsComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.paramsSub.unsubscribe();
     this.partySub.unsubscribe();
+    this.uninvitedSub.unsubscribe();
   }
 }
