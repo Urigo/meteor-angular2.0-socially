ed69189d8691a05295287f2faacbe78536efc2b9
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-23 17:28:59 +0200

    Step 14.3: Use it on the server

--- server/main.ts
+++ server/main.ts
@@ -2,7 +2,8 @@ import { Meteor } from 'meteor/meteor';
 
 import { loadParties } from './imports/fixtures/parties';
 
-import './imports/publications/parties'; 
+import './imports/publications/parties';
+import './imports/publications/users'; 
 
 Meteor.startup(() => {
   loadParties();
