6a8083d7faf4dad68c623b4d3de68d2a460957f5
Kamil Kisiela <kamil.kisiela@gmail.com>
2016-09-23 17:27:03 +0200

    Step 14.1: Add uninvited users publication

---
+++ server/imports/publications/users.ts
@@ -0,0 +1,18 @@
+import { Meteor } from 'meteor/meteor';
+
+import { Parties } from '../../../both/collections/parties.collection';
+
+Meteor.publish('uninvited', function (partyId: string) {
+  const party = Parties.findOne(partyId);
+
+  if (!party) {
+    throw new Meteor.Error('404', 'No such party!');
+  }
+
+  return Meteor.users.find({
+    _id: {
+      $nin: party.invited || [],
+      $ne: this.userId
+    }
+  });
+});
