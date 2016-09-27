c30cf886a9f91dd06798ceb0635bac3b624aa570
dotansimha <dotansimha@gmail.com>
2016-09-27 13:35:54 +0300

    Step 19.17: Add auth logic to the App component

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 import template from './app.component.html';
 import style from './app.component.scss';
+import {InjectUser} from "angular2-meteor-accounts-ui";
 
 @Component({
   selector: 'app',
   template,
   styles: [ style ]
 })
-export class AppComponent {}
+@InjectUser('user')
+export class AppComponent {
+  constructor() {
+
+  }
+
+  logout() {
+    Meteor.logout();
+  }
+}
