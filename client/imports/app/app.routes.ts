268685f25f93c6967d476d209fe2a298e1d41230
dotansimha <dotansimha@gmail.com>
2016-09-27 13:51:05 +0300

    Step 19.21: Added signup route

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -4,11 +4,13 @@ import { Meteor } from 'meteor/meteor';
 import { PartiesListComponent } from './parties/parties-list.component';
 import { PartyDetailsComponent } from './parties/party-details.component';
 import {LoginComponent} from "./auth/login.component";
+import {SignupComponent} from "./auth/singup.component";
 
 export const routes: Route[] = [
   { path: '', component: PartiesListComponent },
   { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['canActivateForLoggedIn'] },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 export const ROUTES_PROVIDERS = [{
