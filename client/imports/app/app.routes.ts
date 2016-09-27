d9bd1ccb22decb9abda289f3e78ec16e55854f64
dotansimha <dotansimha@gmail.com>
2016-09-27 13:54:19 +0300

    Step 19.25: Added the recover route

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -5,12 +5,14 @@ import { PartiesListComponent } from './parties/parties-list.component';
 import { PartyDetailsComponent } from './parties/party-details.component';
 import {LoginComponent} from "./auth/login.component";
 import {SignupComponent} from "./auth/singup.component";
+import {RecoverComponent} from "./auth/recover.component";
 
 export const routes: Route[] = [
   { path: '', component: PartiesListComponent },
   { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['canActivateForLoggedIn'] },
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'recover', component: RecoverComponent }
 ];
 
 export const ROUTES_PROVIDERS = [{
