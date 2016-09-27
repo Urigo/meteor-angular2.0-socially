75575a06c6e3f239263f0df43a593741570838a1
dotansimha <dotansimha@gmail.com>
2016-09-27 10:25:20 +0300

    Step 15.3: Import methods on the client side

--- client/main.ts
+++ client/main.ts
@@ -4,5 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './imports/app/app.module';
 
+import '../both/methods/parties.methods';
+
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
