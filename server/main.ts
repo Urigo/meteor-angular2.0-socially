a6812d1f6bf91e2374b377157e081544213a5dfc
dotansimha <dotansimha@gmail.com>
2016-09-27 10:24:34 +0300

    Step 15.2: Import methods on the server side

--- server/main.ts
+++ server/main.ts
@@ -3,7 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import { loadParties } from './imports/fixtures/parties';
 
 import './imports/publications/parties';
-import './imports/publications/users'; 
+import './imports/publications/users';
+import '../both/methods/parties.methods';
 
 Meteor.startup(() => {
   loadParties();
