462a0f9b0831a2824291a5d1899463cfffd06357
dotansimha <dotansimha@gmail.com>
2016-09-27 13:50:49 +0300

    Step 19.25: Added signup component to the index file

--- client/imports/app/auth/index.ts
+++ client/imports/app/auth/index.ts
@@ -1,5 +1,7 @@
 import {LoginComponent} from "./login.component";
+import {SignupComponent} from "./signup.component";
 
 export const AUTH_DECLARATIONS = [
-  LoginComponent
+  LoginComponent,
+  SignupComponent
 ];
