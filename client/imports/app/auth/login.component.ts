0c69a5a030a73703f8ed453d1171cd9fcfc6d242
dotansimha <dotansimha@gmail.com>
2016-09-27 13:46:12 +0300

    Step 19.18: Create LoginComponent

---
+++ client/imports/app/auth/login.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit, NgZone} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Meteor } from 'meteor/meteor';
+
+import template from './login.component.html';
+
+@Component({
+  selector: 'login',
+  template
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  error: string;
+
+  constructor(private router: Router, private zone: NgZone, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.error = '';
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      Meteor.loginWithPassword(this.loginForm.value.email, this.loginForm.value.password, (err) => {
+        if (err) {
+          this.zone.run(() => {
+            this.error = err;
+          });
+        } else {
+          this.router.navigate(['/']);
+        }
+      });
+    }
+  }
+}
