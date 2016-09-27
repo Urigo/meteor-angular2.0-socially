3747ae07233765ed0230e64d997de2931580df24
dotansimha <dotansimha@gmail.com>
2016-09-27 14:35:15 +0300

    Step 21.25: Import those publications in the server entry point

--- server/main.ts
+++ server/main.ts
@@ -5,6 +5,7 @@ import { loadParties } from './imports/fixtures/parties';
 import './imports/publications/parties';
 import './imports/publications/users';
 import '../both/methods/parties.methods';
+import './imports/publications/images';
 
 Meteor.startup(() => {
   loadParties();
