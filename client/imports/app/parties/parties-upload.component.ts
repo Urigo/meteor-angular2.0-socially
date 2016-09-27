30e6f109dd38f32bf7dd9a317e831742a7813aa5
dotansimha <dotansimha@gmail.com>
2016-09-27 14:25:46 +0300

    Step 21.14: Create a PartiesUpload component

---
+++ client/imports/app/parties/parties-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import template from './parties-upload.component.html';
+
+@Component({
+  selector: 'parties-upload',
+  template
+})
+export class PartiesUploadComponent {
+  constructor() {}
+}
