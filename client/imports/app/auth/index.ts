e433ed9797dfae289ed38e2440b216b61c47a969
dotansimha <dotansimha@gmail.com>
2016-09-27 16:46:50 +0300

    Step 22.14: Added MobileLoginComponent to the index file

--- client/imports/app/auth/index.ts
+++ client/imports/app/auth/index.ts
@@ -1,9 +1,11 @@
 import {LoginComponent} from "./login.component.web";
 import {SignupComponent} from "./singup.component";
 import {RecoverComponent} from "./recover.component";
+import {MobileLoginComponent} from "./login.component.mobile";
 
 export const AUTH_DECLARATIONS = [
   LoginComponent,
   SignupComponent,
-  RecoverComponent
+  RecoverComponent,
+  MobileLoginComponent
 ];
