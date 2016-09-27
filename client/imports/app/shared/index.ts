2a36e4eaff1fe56e80c4549f08f9f10c97be6816
dotansimha <dotansimha@gmail.com>
2016-09-27 16:14:26 +0300

    Step 21.38: Add DisplayMainImage pipe

--- client/imports/app/shared/index.ts
+++ client/imports/app/shared/index.ts
@@ -1,7 +1,9 @@
 import { DisplayNamePipe } from './display-name.pipe';
 import {RsvpPipe} from "./rsvp.pipe";
+import {DisplayMainImagePipe} from "./display-main-image.pipe";
 
 export const SHARED_DECLARATIONS: any[] = [
   DisplayNamePipe,
-  RsvpPipe
+  RsvpPipe,
+  DisplayMainImagePipe
 ];
