1ace09adb278e971e0b6f178412b1ccad51a7a58
dotansimha <dotansimha@gmail.com>
2016-09-27 14:25:59 +0300

    Step 21.15: Added  PartiesUpload component to the index file

--- client/imports/app/parties/index.ts
+++ client/imports/app/parties/index.ts
@@ -1,9 +1,11 @@
 import { PartiesFormComponent } from './parties-form.component';
 import { PartiesListComponent } from './parties-list.component';
 import { PartyDetailsComponent } from './party-details.component';
+import {PartiesUploadComponent} from "./parties-upload.component";
 
 export const PARTIES_DECLARATIONS = [
   PartiesFormComponent,
   PartiesListComponent,
-  PartyDetailsComponent
+  PartyDetailsComponent,
+  PartiesUploadComponent
 ];
