dfe9668b256055255bbffdf42d90c1627ec0e5e5
dotansimha <dotansimha@gmail.com>
2016-09-27 16:37:42 +0300

    Step 22.6: Update the index file

--- client/imports/app/auth/index.ts
+++ client/imports/app/auth/index.ts
@@ -1,4 +1,4 @@
-import {LoginComponent} from "./login.component";
+import {LoginComponent} from "./login.component.web";
 import {SignupComponent} from "./singup.component";
 import {RecoverComponent} from "./recover.component";
 
