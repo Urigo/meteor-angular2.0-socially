0d5cdd85f7248ec0dde97eaadf3c78417133fabe
dotansimha <dotansimha@gmail.com>
2016-09-27 13:46:33 +0300

    Step 19.15: Add the login route

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Meteor } from 'meteor/meteor';
 
 import { PartiesListComponent } from './parties/parties-list.component';
 import { PartyDetailsComponent } from './parties/party-details.component';
+import {LoginComponent} from "./auth/login.component";
 
 export const routes: Route[] = [
   { path: '', component: PartiesListComponent },
-  { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['canActivateForLoggedIn'] }
+  { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['canActivateForLoggedIn'] },
+  { path: 'login', component: LoginComponent }
 ];
 
 export const ROUTES_PROVIDERS = [{
