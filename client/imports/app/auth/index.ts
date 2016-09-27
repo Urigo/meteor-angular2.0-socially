04625d9f9edb315c1f5b2d629e2e07f04f3504cc
dotansimha <dotansimha@gmail.com>
2016-09-27 13:54:09 +0300

    Step 19.24: Added the recover component to the index file

--- client/imports/app/auth/index.ts
+++ client/imports/app/auth/index.ts
@@ -1,7 +1,9 @@
 import {LoginComponent} from "./login.component";
 import {SignupComponent} from "./singup.component";
+import {RecoverComponent} from "./recover.component";
 
 export const AUTH_DECLARATIONS = [
   LoginComponent,
-  SignupComponent
+  SignupComponent,
+  RecoverComponent
 ];
