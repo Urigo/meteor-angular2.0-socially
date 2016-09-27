88db71862f031126a4ff08dbde466aff0914e338
dotansimha <dotansimha@gmail.com>
2016-09-27 11:49:10 +0300

    Step 17.2: Change initial parties accordingly

--- server/imports/fixtures/parties.ts
+++ server/imports/fixtures/parties.ts
@@ -6,17 +6,23 @@ export function loadParties() {
     const parties: Party[] = [{
       name: 'Dubstep-Free Zone',
       description: 'Can we please just for an evening not listen to dubstep.',
-      location: 'Palo Alto',
+      location: {
+        name: 'Palo Alto'
+      },
       public: true
     }, {
       name: 'All dubstep all the time',
       description: 'Get it on!',
-      location: 'Palo Alto',
+      location: {
+        name: 'Palo Alto'
+      },
       public: true
     }, {
       name: 'Savage lounging',
       description: 'Leisure suit required. And only fiercest manners.',
-      location: 'San Francisco',
+      location: {
+        name: 'San Francisco'
+      },
       public: false
     }];
 
