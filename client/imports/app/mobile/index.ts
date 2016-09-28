de2316a8eb2c7d655ede4ac1c35680d8c6b2807e
dotansimha <dotansimha@gmail.com>
2016-09-28 13:08:28 +0300

    Step 23.23: Added PartiesListMobile component to the index file

--- client/imports/app/mobile/index.ts
+++ client/imports/app/mobile/index.ts
@@ -1,5 +1,7 @@
 import {AppMobileComponent} from "./app.component.mobile";
+import {PartiesListMobileComponent} from "./parties-list.component.mobile";
 
 export const MOBILE_DECLARATIONS = [
-  AppMobileComponent
+  AppMobileComponent,
+  PartiesListMobileComponent
 ];
