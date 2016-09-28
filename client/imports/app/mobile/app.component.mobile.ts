4f74e9b9cea410bf47a689cbffe78583afea00b5
dotansimha <dotansimha@gmail.com>
2016-09-28 12:59:06 +0300

    Step 23.16: Imported the main css file of ionic

--- client/imports/app/mobile/app.component.mobile.ts
+++ client/imports/app/mobile/app.component.mobile.ts
@@ -2,6 +2,10 @@ import {Component} from "@angular/core";
 import template from "./app.component.mobile.html";
 import {MenuController, Platform, App} from "ionic-angular";
 
+if (Meteor.isCordova) {
+  require("ionic-angular/css/ionic.css");
+}
+
 @Component({
   selector: "app",
   template
