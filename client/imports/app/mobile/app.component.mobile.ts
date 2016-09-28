387822ba42dc09f0e2f1be05f827e52727b80af3
dotansimha <dotansimha@gmail.com>
2016-09-28 13:14:30 +0300

    Step 23.24: Added the rootPage

--- client/imports/app/mobile/app.component.mobile.ts
+++ client/imports/app/mobile/app.component.mobile.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import template from "./app.component.mobile.html";
 import {MenuController, Platform, App} from "ionic-angular";
+import {PartiesListMobileComponent} from "./parties-list.component.mobile";
 
 if (Meteor.isCordova) {
   require("ionic-angular/css/ionic.css");
@@ -11,7 +12,9 @@ if (Meteor.isCordova) {
   template
 })
 export class AppMobileComponent {
-  constructor(app: App, platform: Platform, menu: MenuController) {
+  rootPage: any;
 
+  constructor(app: App, platform: Platform, menu: MenuController) {
+    this.rootPage = PartiesListMobileComponent;
   }
 }
