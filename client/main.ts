6ef409ea1d92e7a56829fef91a55e2d853410fa4
dotansimha <dotansimha@gmail.com>
2016-09-28 12:39:16 +0300

    Step 23.12: Use ionic-selector package

--- client/main.ts
+++ client/main.ts
@@ -6,7 +6,13 @@ import { AppModule } from './imports/app/app.module';
 
 import '../both/methods/parties.methods';
 
+import ionicSelector from 'ionic-selector';
+
 Meteor.startup(() => {
+  if (Meteor.isCordova) {
+    ionicSelector("app");
+  }
+
   const platform = platformBrowserDynamic();
   platform.bootstrapModule(AppModule);
 });
