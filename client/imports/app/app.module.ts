73a62ea837c70bc371cad7d4c8b5c0616ce65cde
dotansimha <dotansimha@gmail.com>
2016-09-28 12:17:32 +0300

    Step 23.10: Imported mobile declarations and added conditional main component bootstrap

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -13,32 +13,60 @@ import { SHARED_DECLARATIONS } from './shared';
 import { MaterialModule } from "@angular/material";
 import { AUTH_DECLARATIONS } from "./auth/index";
 import { FileDropModule } from "angular2-file-drop";
+import { MOBILE_DECLARATIONS } from "./mobile/index";
+import { AppMobileComponent } from "./mobile/app.component.mobile";
+import { IonicModule, IonicApp } from "ionic-angular";
 
-@NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule.forRoot(routes),
-    AccountsModule,
-    Ng2PaginationModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyAWoBdZHCNh5R-hB5S5ZZ2oeoYyfdDgniA'
-    }),
-    MaterialModule.forRoot(),
-    FileDropModule
-  ],
-  declarations: [
-    AppComponent,
-    ...PARTIES_DECLARATIONS,
-    ...SHARED_DECLARATIONS,
-    ...AUTH_DECLARATIONS
-  ],
-  providers: [
-    ...ROUTES_PROVIDERS
-  ],
-  bootstrap: [
-    AppComponent
-  ]
-})
+let moduleDefinition;
+
+if (Meteor.isCordova) {
+  moduleDefinition = {
+    imports: [
+      IonicModule.forRoot(AppMobileComponent)
+    ],
+    declarations: [
+      ...SHARED_DECLARATIONS,
+      ...MOBILE_DECLARATIONS
+    ],
+    providers: [
+    ],
+    bootstrap: [
+      IonicApp
+    ],
+    entryComponents: [
+      AppMobileComponent
+    ]
+  }
+}
+else {
+  moduleDefinition = {
+    imports: [
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot(routes),
+      AccountsModule,
+      Ng2PaginationModule,
+      AgmCoreModule.forRoot({
+        apiKey: 'AIzaSyAWoBdZHCNh5R-hB5S5ZZ2oeoYyfdDgniA'
+      }),
+      MaterialModule.forRoot(),
+      FileDropModule
+    ],
+    declarations: [
+      AppComponent,
+      ...PARTIES_DECLARATIONS,
+      ...SHARED_DECLARATIONS,
+      ...AUTH_DECLARATIONS
+    ],
+    providers: [
+      ...ROUTES_PROVIDERS
+    ],
+    bootstrap: [
+      AppComponent
+    ]
+  }
+}
+
+@NgModule(moduleDefinition)
 export class AppModule {}
