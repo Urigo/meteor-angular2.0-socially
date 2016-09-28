3f6bd7c9433933b9abf49539d3d7d3afb03fb4c0
dotansimha <dotansimha@gmail.com>
2016-09-28 12:08:40 +0300

    Step 23.4: Updated the template import

--- client/imports/app/app.component.web.ts
+++ client/imports/app/app.component.web.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import template from './app.component.html';
+import template from './app.component.web.html';
 import {InjectUser} from "angular2-meteor-accounts-ui";
 
 @Component({
