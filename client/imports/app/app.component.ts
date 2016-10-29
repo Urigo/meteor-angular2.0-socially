9db0d801dbbfc4adbba6ac3adb79fdb4ca3b5b89
Dotan Simha <dotansimha@gmail.com>
2016-10-29 12:05:00 +0300

    Step 19.7: Import app component style

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 import template from './app.component.html';
+import style from './app.component.scss';
 
 @Component({
   selector: 'app',
-  template
+  template,
+  styles: [ style ]
 })
 export class AppComponent {}
